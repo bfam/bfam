3819b0db72f582d53896c057cea44681a880b25e
Lucas C Wilcox <lucas@swirlee.com>
2013-01-15 16:30:43 -0800

    Use platform check to include sysexits.h

--- src/bfam.h
+++ src/bfam.h
@@ -6,6 +6,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#if defined (BFAM_HAVE_SYSEXITS_H)
+#include <sysexits.h>
+#elif defined (BFAM_HAVE_SYS_SYSEXITS_H)
+#include <sys/sysexits.h>
+#else
+#define EX_OSERR EXIT_FAILURE
+#define EX_USAGE EXIT_FAILURE
+#endif
+
 #include <mpi.h>
 
 #include <bfam_version.h>
