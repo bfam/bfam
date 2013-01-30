41a4eabf47e9a448fd4aeb6962b0905d241d3823
Jeremy E Kozdon <jekozdon@nps.edu>
2013-01-29 18:25:55 -0800

    adding comm to subdomain

--- src/bfam_subdomain.h
+++ src/bfam_subdomain.h
@@ -1,6 +1,8 @@
 #ifndef BFAM_SUBDOMAIN_H
 #define BFAM_SUBDOMAIN_H
 
+#include <bfam_mpicomm.h>
+
 /**
  * base structure for all subdomains types. Any new subdomain should have this
  * as its first member with the name base, i.e.,
@@ -13,6 +15,9 @@
  */
 typedef struct bfam_subdomain
 {
+  // comm
+  char *name; /**< Name of the subdomain */
+  bfam_mpicomm_t * comm; /**< communicator for this subdomain */
 } bfam_subdomain_t;
 
 /**
@@ -20,10 +25,11 @@ typedef struct bfam_subdomain
  */
 typedef struct domain 
 {
-  bfam_subdomain_t * domain; /**< array of pointers to subdomains */ int
-  numSubdomains;             /**< number of subdomains that are currently in the domain */
+  bfam_subdomain_t * domain; /**< array of pointers to subdomains */
+  int numSubdomains;         /**< number of subdomains that are currently in the domain */
   int sizeSubdomains;        /**< total number of subdomains the domain can
                                hold, i.e.  size of the array*/
+  bfam_mpicomm_t * comm; /**< communicator for the domain */
 } domain_t;
 
 #endif
