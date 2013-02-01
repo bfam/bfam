7acc0ff9ba7286f6deab1c30c76ee25429644e64
Jeremy E Kozdon <jekozdon@nps.edu>
2013-02-01 13:25:31 -0800

    Removing some comment markers

--- src/bfam_subdomain.h
+++ src/bfam_subdomain.h
@@ -16,7 +16,6 @@
  */
 typedef struct bfam_subdomain
 {
-  // comm
   char*           name;     /**< Name of the subdomain */
   bfam_mpicomm_t* comm;     /**< communicator for this subdomain */
   int             hasWorkl; /**< boolean for whether or not I can do work on this processor */
@@ -47,31 +46,31 @@ typedef struct bfam_domain
   bfam_critbit0_tree_t name2num; /**< critbit map for convertings subdomain names to numbers */
 } bfam_domain_t;
 
-// /** create a domain
-//  * 
-//  * \param [out] newDomain the newly created domain
-//  * \param [in]  domComm   pointer to the communicator for the domain
-//  */
+/** create a domain
+ * 
+ * \param [out] newDomain the newly created domain
+ * \param [in]  domComm   pointer to the communicator for the domain
+ */
 bfam_domain_t* bfam_domain_new(bfam_mpicomm_t *domComm);
 
-// /** initializes a domain
-//  * 
-//  * \param [in,out] domain pointer to the domain
-//  * \param [in]     domComm pointer to the communicator for the domain
-//  */
+/** initializes a domain
+ * 
+ * \param [in,out] domain pointer to the domain
+ * \param [in]     domComm pointer to the communicator for the domain
+ */
 void bfam_domain_init(bfam_domain_t *domain, bfam_mpicomm_t *domComm);
 
-// /** free up the memory allocated by the domain
-//  * 
-//  * \param [in,out] domain domain to clean up
-//  */
+/** free up the memory allocated by the domain
+ * 
+ * \param [in,out] domain domain to clean up
+ */
 void bfam_domain_free(bfam_domain_t *domain);
 
-// /** Add subdomain
-//  * 
-//  * \param [in,out] thisDomain domain to add subdomain to
-//  * \param [in]     newSubdomain subdomain to add to the domain
-//  */
+/** Add subdomain
+ * 
+ * \param [in,out] thisDomain domain to add subdomain to
+ * \param [in]     newSubdomain subdomain to add to the domain
+ */
 void bfam_domain_add_subdomain(bfam_domain_t *thisDomain, bfam_subdomain_t *newSubdomain);
 
 #endif
