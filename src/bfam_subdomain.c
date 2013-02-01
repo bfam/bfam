c61b5b6f696201b278b6147a5abcdc37e7bab2ed
Jeremy E Kozdon <jekozdon@nps.edu>
2013-02-01 12:11:59 -0800

    modified to be an init and a new function for domain.

--- src/bfam_subdomain.c
+++ src/bfam_subdomain.c
@@ -1,7 +1,14 @@
 #include <bfam_subdomain.h>
 #include <bfam_base.h>
 
-void bfam_domain_new(bfam_domain_t *thisDomain, bfam_mpicomm_t *domComm)
+bfam_domain_t* bfam_domain_new(bfam_mpicomm_t *domComm)
+{
+  bfam_domain_t* newDomain = bfam_malloc(sizeof(bfam_domain_t));
+  bfam_domain_init(newDomain,domComm);
+  return newDomain;
+}
+
+void bfam_domain_init(bfam_domain_t *thisDomain, bfam_mpicomm_t *domComm)
 {
   const int sizeSubdomains = 16;
   thisDomain->comm = domComm; // Perhaps we should duplicate it?
@@ -17,4 +24,23 @@ void bfam_domain_free(bfam_domain_t *thisDomain)
   thisDomain->sizeSubdomains = 0;
   bfam_free(thisDomain->subdomains);
   thisDomain->subdomains = NULL;
+}
+
+void bfam_domain_add_subdomain(bfam_domain_t* thisDomain, bfam_subdomain_t* newSubdomain)
+{
+  // double size
+  if(thisDomain->numSubdomains == thisDomain->sizeSubdomains)
+  {
+    bfam_subdomain_t** oldSubdomains = thisDomain->subdomains;
+    thisDomain->sizeSubdomains = 2*thisDomain->sizeSubdomains;
+    thisDomain->subdomains =
+      bfam_malloc(thisDomain->sizeSubdomains*sizeof(bfam_subdomain_t*));
+    for(int i = 0;i < thisDomain->numSubdomains;i++)
+      thisDomain->subdomains[i] = oldSubdomains[i];
+    bfam_free(oldSubdomains);
+  }
+
+  // Add next domain
+  thisDomain->subdomains[thisDomain->numSubdomains] = newSubdomain;
+  thisDomain->numSubdomains++;
 }
