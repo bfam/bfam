d5da1b418feef3d3ec842fd4763aec81b5815334
Lucas C Wilcox <lucas@swirlee.com>
2013-05-31 16:09:54 -0700

    Use a p4est domain in the dgquad mesh example

--- examples/mesh/bfam_exam_mesh_dgquad.c
+++ examples/mesh/bfam_exam_mesh_dgquad.c
@@ -30,32 +30,32 @@ refine_fn(p4est_t * p4est, p4est_topidx_t which_tree,
 int
 main (int argc, char *argv[])
 {
-  MPI_Comm comm = MPI_COMM_WORLD;
-  p4est_t *p4est;
+  bfam_mpicomm_t comm = { .comm = MPI_COMM_WORLD, .isMember = 1 };
   p4est_connectivity_t *conn;
   int rank;
 
   BFAM_MPI_CHECK(MPI_Init(&argc,&argv));
-  BFAM_MPI_CHECK(MPI_Comm_rank(comm, &rank));
+  BFAM_MPI_CHECK(MPI_Comm_rank(comm.comm, &rank));
 
   bfam_log_init(rank, stdout, BFAM_LL_DEFAULT);
   bfam_signal_handler_set();
 
-  sc_init(comm, 0, 0, NULL, SC_LP_DEFAULT);
+  sc_init(comm.comm, 0, 0, NULL, SC_LP_DEFAULT);
   p4est_init(NULL, SC_LP_DEFAULT);
 
   conn = p4est_connectivity_new_corner();
-  p4est = p4est_new_ext(comm, conn, 0, 0, 0, 0, NULL, NULL);
 
+  bfam_domain_p4est_t* domain = bfam_domain_p4est_new(&comm, conn);
 
   refine_level = 1;
-  p4est_refine(p4est, 1, refine_fn, NULL);
-  p4est_balance(p4est, P4EST_CONNECT_FACE, NULL);
-  p4est_partition(p4est, NULL);
+  p4est_refine(domain->p4est, 1, refine_fn, NULL);
+  p4est_balance(domain->p4est, P4EST_CONNECT_FACE, NULL);
+  p4est_partition(domain->p4est, NULL);
 
-  p4est_vtk_write_file(p4est, NULL, "p4est_mesh");
+  p4est_vtk_write_file(domain->p4est, NULL, "p4est_mesh");
 
-  p4est_destroy(p4est);
+  bfam_domain_p4est_free(domain);
+  bfam_free(domain);
   p4est_connectivity_destroy(conn);
 
   sc_finalize();
