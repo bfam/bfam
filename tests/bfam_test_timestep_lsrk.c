556d4bad0f9175a70dcdeb920ca8bae44bd35dcd
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-06-05 20:54:43 -0700

    empty domain added to test

--- tests/bfam_test_timestep_lsrk.c
+++ tests/bfam_test_timestep_lsrk.c
@@ -4,22 +4,25 @@ int
 main (int argc, char *argv[])
 {
   bfam_ts_lsrk_t* ts;
+  bfam_domain_t* dom = bfam_domain_new(NULL);
 
   /* test Kennedy-Carpenter(5,4) */
-  ts = bfam_ts_lsrk_new(NULL,BFAM_TS_LSRK_KC54);
+  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_KC54);
   bfam_ts_lsrk_free(ts);
 
   /* test Williamson(3,3) */
-  ts = bfam_ts_lsrk_new(NULL,BFAM_TS_LSRK_W33);
+  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_W33);
   bfam_ts_lsrk_free(ts);
 
   /* test Heun */
-  ts = bfam_ts_lsrk_new(NULL,BFAM_TS_LSRK_HEUN);
+  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_HEUN);
   bfam_ts_lsrk_free(ts);
 
   /* test Forward Euler */
-  ts = bfam_ts_lsrk_new(NULL,BFAM_TS_LSRK_FE);
+  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_FE);
   bfam_ts_lsrk_free(ts);
+
+  bfam_domain_free(dom);
 
   return EXIT_SUCCESS;
 }
