d31d319203e90b453f145c80932dda4e747bf0e5
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-06-05 19:36:26 -0700

    adding RK values

--- src/bfam_timestep_lsrk.c
+++ src/bfam_timestep_lsrk.c
@@ -19,12 +19,77 @@ bfam_ts_lsrk_init(bfam_ts_lsrk_t* ts, bfam_domain_t* dom,
   switch(method)
   {
     case BFAM_TS_LSRK_KC54:
-      break;
-    case BFAM_TS_LSRK_FE:
-      break;
-    case BFAM_TS_LSRK_HEUN:
+      ts->nStages = 5;
+      ts->A = bfam_malloc_aligned(ts->nStages*sizeof(bfam_real_t));
+      ts->B = bfam_malloc_aligned(ts->nStages*sizeof(bfam_real_t));
+      ts->C = bfam_malloc_aligned((ts->nStages+1)*sizeof(bfam_real_t));
+
+      ts->A[0] = 0;
+      ts->A[1] = -567301805773.0/1357537059087.0;
+      ts->A[2] = -2404267990393.0/2016746695238.0;
+      ts->A[3] = -3550918686646.0/2091501179385.0;
+      ts->A[4] = -1275806237668.0/842570457699.0;
+
+      ts->B[0] = 1432997174477.0/9575080441755.0;
+      ts->B[1] = 5161836677717.0/13612068292357.0;
+      ts->B[2] = 1720146321549.0/2090206949498.0;
+      ts->B[3] = 3134564353537.0/4481467310338.0;
+      ts->B[4] = 2277821191437.0/14882151754819.0;
+
+      ts->C[0] = 0.0;
+      ts->C[1] = 1432997174477.0/9575080441755.0;
+      ts->C[2] = 2526269341429.0/6820363962896.0;
+      ts->C[3] = 2006345519317.0/3224310063776.0;
+      ts->C[4] = 2802321613138.0/2924317926251.0;
+      ts->C[5] = 1.0;
       break;
     case BFAM_TS_LSRK_W33:
+      ts->nStages = 3;
+      ts->A = bfam_malloc_aligned(ts->nStages*sizeof(bfam_real_t));
+      ts->B = bfam_malloc_aligned(ts->nStages*sizeof(bfam_real_t));
+      ts->C = bfam_malloc_aligned((ts->nStages+1)*sizeof(bfam_real_t));
+
+      ts->A[0] = 0.0;
+      ts->A[1] = -5.0/9.0;
+      ts->A[2] = -153.0/128.0;
+
+      ts->B[0] = 1.0/3.0;
+      ts->B[1] = 15.0/16.0;
+      ts->B[2] = 8.0/15.0;
+
+      ts->C[0] = 0.0;
+      ts->C[1] = 1.0/3.0;
+      ts->C[2] = 3.0/4.0;
+      ts->C[3] = 1.0;
+      break;
+    case BFAM_TS_LSRK_HEUN:
+      ts->nStages = 2;
+      ts->A = bfam_malloc_aligned(ts->nStages*sizeof(bfam_real_t));
+      ts->B = bfam_malloc_aligned(ts->nStages*sizeof(bfam_real_t));
+      ts->C = bfam_malloc_aligned((ts->nStages+1)*sizeof(bfam_real_t));
+
+      ts->A[0] = 0.0;
+      ts->A[1] = -1.0;
+
+      ts->B[0] = 1.0;
+      ts->B[1] = 1.0/2.0;
+
+      ts->C[0] = 0.0;
+      ts->C[1] = 1.0;
+      ts->C[2] = 1.0;
+      break;
+    case BFAM_TS_LSRK_FE:
+      ts->nStages = 1;
+      ts->A = bfam_malloc_aligned(ts->nStages*sizeof(bfam_real_t));
+      ts->B = bfam_malloc_aligned(ts->nStages*sizeof(bfam_real_t));
+      ts->C = bfam_malloc_aligned((ts->nStages+1)*sizeof(bfam_real_t));
+
+      ts->A[0] = 0.0;
+
+      ts->B[0] = 1.0;
+
+      ts->C[0] = 0.0;
+      ts->C[1] = 1.0;
       break;
   }
 }
@@ -32,9 +97,9 @@ bfam_ts_lsrk_init(bfam_ts_lsrk_t* ts, bfam_domain_t* dom,
 void
 bfam_ts_lsrk_free(bfam_ts_lsrk_t* ts)
 {
-  bfam_free(ts->A);
-  bfam_free(ts->B);
-  bfam_free(ts->C);
+  bfam_free_aligned(ts->A);
+  bfam_free_aligned(ts->B);
+  bfam_free_aligned(ts->C);
   ts->nStages = 0;
   ts->t  = NAN;
   ts->dt = NAN;
