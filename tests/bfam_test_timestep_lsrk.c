f3b76ff03112f8d2f411a9eb93d0cf60cc66c8b9
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-06-05 20:22:11 -0700

    preliminary test calls

--- tests/bfam_test_timestep_lsrk.c
+++ tests/bfam_test_timestep_lsrk.c
@@ -3,5 +3,23 @@
 int
 main (int argc, char *argv[])
 {
+  bfam_ts_lsrk_t* ts;
+
+  /* test Kennedy-Carpenter(5,4) */
+  ts = bfam_ts_lsrk_new(NULL,BFAM_TS_LSRK_KC54);
+  bfam_ts_lsrk_free(ts);
+
+  /* test Williamson(3,3) */
+  ts = bfam_ts_lsrk_new(NULL,BFAM_TS_LSRK_W33);
+  bfam_ts_lsrk_free(ts);
+
+  /* test Heun */
+  ts = bfam_ts_lsrk_new(NULL,BFAM_TS_LSRK_HEUN);
+  bfam_ts_lsrk_free(ts);
+
+  /* test Forward Euler */
+  ts = bfam_ts_lsrk_new(NULL,BFAM_TS_LSRK_FE);
+  bfam_ts_lsrk_free(ts);
+
   return EXIT_SUCCESS;
 }
