1637228943a3e717dda18ae84ae71a4e5ec23699
Jeremy E Kozdon <jekozdon@nps.edu>
2013-06-12 13:41:52 -0700

    function call for timestep

--- tests/bfam_test_timestep_lsrk.c
+++ tests/bfam_test_timestep_lsrk.c
@@ -1,34 +1,54 @@
 #include <bfam.h>
 #include <bfam_subdomain_dummy.h>
 
+void
+test_lsrk(bfam_ts_lsrk_method_t type,int N)
+{
+  bfam_ts_lsrk_t* ts;
+
+  bfam_domain_t* dom = bfam_domain_new(NULL);
+  bfam_subdomain_dummy_t* subDom = NULL;
+
+  subDom = bfam_subdomain_dummy_new("a",N);
+  bfam_subdomain_add_tag((bfam_subdomain_t*)subDom,"yes");
+  bfam_subdomain_add_tag((bfam_subdomain_t*)subDom,"other");
+  bfam_subdomain_add_tag((bfam_subdomain_t*)subDom,"dummy");
+  bfam_domain_add_subdomain(dom, (bfam_subdomain_t*) subDom);
+
+  subDom = bfam_subdomain_dummy_new("b",N);
+  bfam_subdomain_add_tag((bfam_subdomain_t*)subDom,"no");
+  bfam_subdomain_add_tag((bfam_subdomain_t*)subDom,"other");
+  bfam_domain_add_subdomain(dom, (bfam_subdomain_t*) subDom);
+
+  subDom = bfam_subdomain_dummy_new("c",N);
+  bfam_subdomain_add_tag((bfam_subdomain_t*)subDom,"dummy");
+  bfam_subdomain_add_tag((bfam_subdomain_t*)subDom,"yes");
+  bfam_subdomain_add_tag((bfam_subdomain_t*)subDom,"random");
+  bfam_domain_add_subdomain(dom, (bfam_subdomain_t*) subDom);
+
+  subDom = bfam_subdomain_dummy_new("d",N);
+  bfam_subdomain_add_tag((bfam_subdomain_t*)subDom,"other");
+  bfam_subdomain_add_tag((bfam_subdomain_t*)subDom,"yes");
+  bfam_subdomain_add_tag((bfam_subdomain_t*)subDom,"random");
+  bfam_domain_add_subdomain(dom, (bfam_subdomain_t*) subDom);
+
+  ts = bfam_ts_lsrk_new(dom,type);
+
+  // free
+  bfam_ts_lsrk_free(ts);
+  bfam_free(ts);
+
+  bfam_domain_free(dom);
+  bfam_free(dom);
+}
+
 int
 main (int argc, char *argv[])
 {
-  bfam_ts_lsrk_t* ts;
-  bfam_domain_t* dom = bfam_domain_new(NULL);
-  bfam_subdomain_dummy_t* subDom = bfam_subdomain_dummy_new(0,"a",4);
-  bfam_domain_add_subdomain(dom, (bfam_subdomain_t*) subDom);
-
-  printf("%e\n",(double) bfam_subdomain_dummy_exact(subDom, 1.0));
-  printf("%e\n",(double) bfam_subdomain_dummy_exact(subDom, 0.2));
-
-  /* test Kennedy-Carpenter(5,4) */
-  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_KC54);
-  bfam_ts_lsrk_free(ts);
-
-  /* test Williamson(3,3) */
-  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_W33);
-  bfam_ts_lsrk_free(ts);
-
-  /* test Heun */
-  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_HEUN);
-  bfam_ts_lsrk_free(ts);
-
-  /* test Forward Euler */
-  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_FE);
-  bfam_ts_lsrk_free(ts);
-
-  bfam_domain_free(dom);
+  test_lsrk(BFAM_TS_LSRK_KC54,4);
+  test_lsrk(BFAM_TS_LSRK_W33 ,3);
+  test_lsrk(BFAM_TS_LSRK_HEUN,2);
+  test_lsrk(BFAM_TS_LSRK_FE  ,1);
 
   return EXIT_SUCCESS;
 }
