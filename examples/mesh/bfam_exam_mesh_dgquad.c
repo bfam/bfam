c21f9b7c48c03b5ed38c3048d03a4c689a785a16
Lucas C Wilcox <lucas@swirlee.com>
2013-06-13 14:17:10 -0700

    Provide CLI for DG quad mesh example
    This allows one to increase the verbosity from the command line.

--- examples/mesh/bfam_exam_mesh_dgquad.c
+++ examples/mesh/bfam_exam_mesh_dgquad.c
@@ -213,19 +213,67 @@ main (int argc, char *argv[])
   MPI_Comm comm = MPI_COMM_WORLD;
   int rank;
 
+  void *options= bfam_gopt_sort(&argc, (const char**)argv,
+      bfam_gopt_start(
+        bfam_gopt_option('h', 0,
+                         bfam_gopt_shorts('h', '?'),
+                         bfam_gopt_longs("help", "HELP")),
+        bfam_gopt_option('V', 0,
+                         bfam_gopt_shorts('V'),
+                         bfam_gopt_longs("version")),
+        bfam_gopt_option('v', BFAM_GOPT_REPEAT,
+                         bfam_gopt_shorts('v'),
+                         bfam_gopt_longs("verbose"))
+        )
+      );
+
+  const char *helpText =
+  "\n"
+  "\n"
+  "  there are four possible options to this program, some of which have\n"
+  "  multiple names:\n"
+  "\n"
+  "    -h -? --help --HELP\n"
+  "    -V --version\n"
+  "    -v --verbose  (which may be repeated for more verbosity)\n"
+  "\n";
+
+  if(bfam_gopt(options, 'h'))
+  {
+    /*
+     * if any of the help options was specified
+     */
+    BFAM_ROOT_INFO(helpText);
+    exit(EXIT_SUCCESS);
+  }
+
+  if(bfam_gopt(options, 'V'))
+  {
+    BFAM_ROOT_INFO("BFAM Version: %s", bfam_version_get());
+    BFAM_ROOT_INFO("BFAM Compile Info:\n" BFAM_COMPILE_INFO);
+    exit( EXIT_SUCCESS );
+  }
+
+  int verbosity = bfam_gopt(options, 'v');
+
   BFAM_MPI_CHECK(MPI_Init(&argc,&argv));
   BFAM_MPI_CHECK(MPI_Comm_rank(comm, &rank));
 
-  bfam_log_init(rank, stdout, BFAM_LL_DEFAULT);
+  int logLevel = BFAM_MAX(BFAM_LL_INFO - verbosity, BFAM_LL_ALWAYS);
+
+  bfam_log_init(rank, stdout, logLevel);
   bfam_signal_handler_set();
 
-  sc_init(comm, 0, 0, NULL, SC_LP_DEFAULT);
-  p4est_init(NULL, SC_LP_DEFAULT);
+  int scLogPriorities = BFAM_MAX(SC_LP_STATISTICS - verbosity, SC_LP_ALWAYS);
+  sc_init(comm, 0, 0, NULL, scLogPriorities);
+  p4est_init(NULL, scLogPriorities);
 
   build_mesh(comm);
 
   sc_finalize();
   BFAM_MPI_CHECK(MPI_Finalize());
+
+  bfam_gopt_free(options);
 
   return EXIT_SUCCESS;
 }
