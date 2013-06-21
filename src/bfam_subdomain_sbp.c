a9fc31aa190acdb4c87bff89fb0e518e184625b6
Jeremy E Kozdon <jekozdon@nps.edu>
2013-06-20 17:07:21 -0700

    sbp fields handling. NEED TO FIX field_init

--- src/bfam_subdomain_sbp.c
+++ src/bfam_subdomain_sbp.c
@@ -16,6 +16,47 @@ bfam_subdomain_sbp_new(const bfam_locidx_t     id,
   return newSub;
 }
 
+static int
+bfam_subdomain_sbp_field_add(bfam_subdomain_t *subdomain, const char *name)
+{
+  bfam_subdomain_sbp_t *s = (bfam_subdomain_sbp_t*) subdomain;
+
+  if(bfam_dictionary_get_value_ptr(&s->base.fields,name))
+    return 1;
+
+  size_t fieldSize = 0;
+  for(int d = 0; d < s->dim; d++)
+    fieldSize *= (s->Nl[d]+1+s->Nb[2*d]+s->Nb[2*d+1]);
+
+  bfam_real_t *field = bfam_malloc_aligned(fieldSize);
+
+  int rval = bfam_dictionary_insert_ptr(&s->base.fields, name, field);
+
+  BFAM_ASSERT(rval != 1);
+
+  if(rval == 0)
+    bfam_free_aligned(field);
+
+  return rval;
+}
+
+static void
+bfam_subdomain_sbp_field_init(bfam_subdomain_t *subdomain,
+    const char *name, bfam_real_t time, bfam_subdomain_init_field_t init_field,
+    void *arg)
+{
+  bfam_subdomain_sbp_t *s = (bfam_subdomain_sbp_t*) subdomain;
+
+  bfam_real_t *field = bfam_dictionary_get_value_ptr(&s->base.fields,name);
+
+  BFAM_ABORT_IF(field==NULL, "Init: Field %s not found in subdomain %s",
+      name, subdomain->name);
+
+  // size_t fieldLength = s->Np*s->K;
+
+  // init_field(fieldLength, time, s->x, s->y, s->z, subdomain, arg, field);
+}
+
 void
 bfam_subdomain_sbp_init(bfam_subdomain_sbp_t *subdomain,
                             const bfam_locidx_t     id,
@@ -29,6 +70,8 @@ bfam_subdomain_sbp_init(bfam_subdomain_sbp_t *subdomain,
   bfam_subdomain_init(&subdomain->base, id, name);
   bfam_subdomain_add_tag(&subdomain->base, "_subdomain_sbp");
   subdomain->base.free = bfam_subdomain_sbp_free;
+  subdomain->base.field_add = bfam_subdomain_sbp_field_add;
+  subdomain->base.field_init = bfam_subdomain_sbp_field_init;
 
   subdomain->dim = dim;
 
@@ -51,10 +94,22 @@ bfam_subdomain_sbp_init(bfam_subdomain_sbp_t *subdomain,
   }
 }
 
+static int
+bfam_subdomain_sbp_free_fields(const char * key, void *val,
+    void *arg)
+{
+  bfam_free_aligned(val);
+
+  return 1;
+}
+
 void
 bfam_subdomain_sbp_free(bfam_subdomain_t *subdomain)
 {
   bfam_subdomain_sbp_t *sub = (bfam_subdomain_sbp_t*) subdomain;
+
+  bfam_dictionary_allprefixed_ptr(&sub->base.fields,"",
+      &bfam_subdomain_sbp_free_fields,NULL);
 
   bfam_subdomain_free(subdomain);
 
