0ff095818e402959ac0b512a7078685b9881e12d
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-08-27 18:17:00 -0700

    updating beard_example to be periodic

--- examples/beard/beard_example.lua
+++ examples/beard/beard_example.lua
@@ -1,14 +1,20 @@
-N = 5
-num_subdomains = 3
+N = 3
+num_subdomains = 1
 connectivity = "brick"
 lsrk_method = "KC54"
 problem = "stress free box"
-refine_level = 1;
+refine_level = 3
+max_refine_level = 3
+
+nsteps = 7000
+ndisp  = 10
+
+dt_scale = 0.1
 
 brick_m = 2
 brick_n = 2
-brick_a = 0
-brick_b = 0
+brick_a = 1
+brick_b = 1
 brick_random = 1
 
 cs = 3.464
