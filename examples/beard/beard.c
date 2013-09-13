bbcb6a1c9d4a576d2c8e3f3e7063c9cb09d06be2
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-09-13 15:11:36 -0700

    Reading connectivity and time stepper

--- examples/beard/beard.c
+++ examples/beard/beard.c
@@ -2,6 +2,44 @@
 #include "beard_dgx_rhs.h"
 #include <p4est_iterate.h>
 
+struct conn_table {
+  const char *name;
+  p4est_connectivity_t * (*conn_fn) (void);
+} conn_table[] = {
+  {"unitsquare", &p4est_connectivity_new_unitsquare},
+  {"periodic",   &p4est_connectivity_new_periodic},
+  {"rotwrap",    &p4est_connectivity_new_rotwrap},
+  {"corner",     &p4est_connectivity_new_corner},
+  {"moebius",    &p4est_connectivity_new_moebius},
+  {"star",       &p4est_connectivity_new_star},
+  {NULL,         NULL}
+};
+
+struct lsrk_table {
+  const char *name;
+  bfam_ts_lsrk_method_t lsrk_method;
+} lsrk_table[] = {
+  {"KC54", BFAM_TS_LSRK_KC54},
+  {"FE",   BFAM_TS_LSRK_FE},
+  {"HEUN", BFAM_TS_LSRK_HEUN},
+  {"W33",  BFAM_TS_LSRK_W33},
+  {NULL,   BFAM_TS_LSRK_NOOP},
+};
+
+typedef struct prefs
+{
+  lua_State *L;
+
+  int min_refine_level;
+  int max_refine_level;
+
+  p4est_connectivity_t * (*conn_fn) (void);
+  char conn_name[BFAM_BUFSIZ];
+
+  bfam_ts_lsrk_method_t lsrk_method;
+  char lsrk_name[BFAM_BUFSIZ];
+} prefs_t;
+
 /*
  * Lua helper functions
  */
@@ -21,14 +59,6 @@ lua_get_global_int(lua_State *L, const char *name, int def, int warning)
   return result;
 }
 
-typedef struct prefs
-{
-  lua_State *L;
-
-  int min_refine_level;
-  int max_refine_level;
-} prefs_t;
-
 /*
  * Set up the beard preference file
  */
@@ -47,8 +77,72 @@ new_prefs(const char *prefs_filename)
 
   BFAM_ASSERT(lua_gettop(L)==0);
 
+  /* get the refinement information */
   prefs->min_refine_level = lua_get_global_int(prefs->L,"min_refine_level",0,1);
   prefs->max_refine_level = lua_get_global_int(prefs->L,"max_refine_level",0,1);
+
+  /* get the time stepper type */
+  lua_getglobal(L, "lsrk_method");
+  prefs->lsrk_method = lsrk_table[0].lsrk_method;
+  strncpy(prefs->lsrk_name,lsrk_table[0].name,BFAM_BUFSIZ);
+  if(lua_isstring(L, -1))
+  {
+    int i;
+    const char *lsrk_name = lua_tostring(L, -1);
+    for(i = 0; lsrk_table[i].name != NULL; ++i)
+    {
+      if(strcmp(lsrk_name, lsrk_table[i].name) == 0)
+        break;
+    }
+
+    if(lsrk_table[i].name == NULL)
+      BFAM_ROOT_WARNING("invalid lsrk method name: `%s'; using default %s",
+          lsrk_name, prefs->lsrk_name);
+    else
+    {
+      prefs->lsrk_method = lsrk_table[i].lsrk_method;
+      strncpy(prefs->lsrk_name,lsrk_table[i].name,BFAM_BUFSIZ);
+    }
+  }
+  else
+  {
+    BFAM_ROOT_WARNING("`lsrk method' not found, using default: %s",
+        prefs->lsrk_name);
+  }
+  BFAM_ASSERT(prefs->lsrk_method != BFAM_TS_LSRK_NOOP);
+  lua_pop(L, 1);
+
+  /* get the connectivity type */
+  lua_getglobal(L, "connectivity");
+  prefs->conn_fn = conn_table[0].conn_fn;
+  strncpy(prefs->conn_name,conn_table[0].name,BFAM_BUFSIZ);
+  if(lua_isstring(L, -1))
+  {
+    int i;
+    const char *conn_name = lua_tostring(L, -1);
+
+    for(i = 0; conn_table[i].name != NULL; ++i)
+    {
+      if(strcmp(conn_name, conn_table[i].name) == 0)
+        break;
+    }
+
+    if(conn_table[i].name == NULL)
+      BFAM_ROOT_WARNING("invalid connectivity name: `%s'; using default %s",
+          conn_name, prefs->conn_name);
+    else
+    {
+      prefs->conn_fn = conn_table[i].conn_fn;
+      strncpy(prefs->conn_name,conn_table[i].name,BFAM_BUFSIZ);
+    }
+  }
+  else
+  {
+    BFAM_ROOT_WARNING("`connectivity' not found, using default %s",
+        prefs->conn_name);
+  }
+  BFAM_ASSERT(prefs->conn_fn != NULL);
+  lua_pop(L, 1);
 
   return prefs;
 }
@@ -65,6 +159,9 @@ print_prefs(prefs_t *prefs)
   BFAM_ROOT_INFO("----------Preferences----------");
   BFAM_ROOT_INFO(" minimum refinement level = %d",prefs->min_refine_level);
   BFAM_ROOT_INFO(" maximum refinement level = %d",prefs->max_refine_level);
+  BFAM_ROOT_INFO("");
+  BFAM_ROOT_INFO(" Low Storage Time Stepper = %s",prefs->lsrk_name);
+  BFAM_ROOT_INFO(" Connectivity             = %s",prefs->conn_name);
   BFAM_ROOT_INFO("-------------------------------");
 }
 
