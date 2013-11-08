f3bbacef2abc58af0617236e75ea365a02a4e67d
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-11-08 15:41:25 -0800

    adding tensor and vector communication test

--- tests/bfam_test_mesh_dgquad.c
+++ tests/bfam_test_mesh_dgquad.c
@@ -42,7 +42,7 @@ poly1_field(bfam_locidx_t npoints, const char* name,
   BFAM_ASSUME_ALIGNED(field, 32);
 
   for(bfam_locidx_t n=0; n < npoints; ++n)
-    field[n] = -x[n] - y[n];
+    field[n] = -x[n] - y[n]*x[n]*y[n];
 }
 
 static void
@@ -60,14 +60,75 @@ poly2_field(bfam_locidx_t npoints, const char* name,
     field[n] = x[n] + y[n];
 }
 
+static void
+poly3_field(bfam_locidx_t npoints, const char* name,
+    bfam_real_t time, bfam_real_t *restrict x, bfam_real_t *restrict y,
+    bfam_real_t *restrict z, struct bfam_subdomain *s, void *arg,
+    bfam_real_t *restrict field)
+{
+  BFAM_ASSUME_ALIGNED(x, 32);
+  BFAM_ASSUME_ALIGNED(y, 32);
+  BFAM_ASSUME_ALIGNED(z, 32);
+  BFAM_ASSUME_ALIGNED(field, 32);
+
+  for(bfam_locidx_t n=0; n < npoints; ++n)
+    field[n] = x[n] + y[n]*x[n] + x[n]*x[n]*y[n];
+}
+
+static void
+poly4_field(bfam_locidx_t npoints, const char* name,
+    bfam_real_t time, bfam_real_t *restrict x, bfam_real_t *restrict y,
+    bfam_real_t *restrict z, struct bfam_subdomain *s, void *arg,
+    bfam_real_t *restrict field)
+{
+  BFAM_ASSUME_ALIGNED(x, 32);
+  BFAM_ASSUME_ALIGNED(y, 32);
+  BFAM_ASSUME_ALIGNED(z, 32);
+  BFAM_ASSUME_ALIGNED(field, 32);
+
+  for(bfam_locidx_t n=0; n < npoints; ++n)
+    field[n] = y[n] + y[n]*x[n] + x[n]*x[n]*y[n];
+}
+
+static void
+poly5_field(bfam_locidx_t npoints, const char* name,
+    bfam_real_t time, bfam_real_t *restrict x, bfam_real_t *restrict y,
+    bfam_real_t *restrict z, struct bfam_subdomain *s, void *arg,
+    bfam_real_t *restrict field)
+{
+  BFAM_ASSUME_ALIGNED(x, 32);
+  BFAM_ASSUME_ALIGNED(y, 32);
+  BFAM_ASSUME_ALIGNED(z, 32);
+  BFAM_ASSUME_ALIGNED(field, 32);
+
+  for(bfam_locidx_t n=0; n < npoints; ++n)
+    field[n] = x[n]*y[n] + y[n]*x[n] + x[n]*x[n]*y[n];
+}
+
+static void
+poly6_field(bfam_locidx_t npoints, const char* name,
+    bfam_real_t time, bfam_real_t *restrict x, bfam_real_t *restrict y,
+    bfam_real_t *restrict z, struct bfam_subdomain *s, void *arg,
+    bfam_real_t *restrict field)
+{
+  BFAM_ASSUME_ALIGNED(x, 32);
+  BFAM_ASSUME_ALIGNED(y, 32);
+  BFAM_ASSUME_ALIGNED(z, 32);
+  BFAM_ASSUME_ALIGNED(field, 32);
+
+  for(bfam_locidx_t n=0; n < npoints; ++n)
+    field[n] = x[n]*x[n] + y[n]*y[n] + x[n] + y[n];
+}
+
 static int
-check_pm(bfam_subdomain_dgx_quad_glue_t *sub, const char *name)
+check_pm(bfam_subdomain_dgx_quad_glue_t *sub, const char *name, bfam_real_t fac)
 {
   int failures = 0;
   bfam_real_t *f_m = bfam_dictionary_get_value_ptr(&sub->base.fields_m, name);
   bfam_real_t *f_p = bfam_dictionary_get_value_ptr(&sub->base.fields_p, name);
-  bfam_real_t *sub_m_f   = bfam_dictionary_get_value_ptr(&sub->sub_m->base.fields,
-                                                   name);
+
+  BFAM_ASSERT(f_m != NULL);
+  BFAM_ASSERT(f_p != NULL);
 
   BFAM_LDEBUG("Testing subdomain (%2jd, %2jd) -- (%2jd, %2jd)",
       (intmax_t)sub->rank_m, (intmax_t)sub->s_m,
@@ -80,22 +141,44 @@ check_pm(bfam_subdomain_dgx_quad_glue_t *sub, const char *name)
     for(bfam_locidx_t j = 0; j < sub->Np; ++j)
       BFAM_LDEBUG("fm[%2d][%2d] = %20"BFAM_REAL_PRIe
             "    fp[%2d][%2d] = %20"BFAM_REAL_PRIe,
-          i, j, f_m[i*sub->Np + j], i, j, f_p[i*sub->Np + j]);
+          i, j, f_m[i*sub->Np + j], i, j, fac*f_p[i*sub->Np + j]);
 
     for(int j=0; j<sub->Np; ++j)
     {
       size_t idx = i*sub->Np + j;
-      int fail = !REAL_APPROX_EQ(f_m[idx], f_p[idx], 10);
+      int fail = !REAL_APPROX_EQ(f_m[idx], fac*f_p[idx], 10);
 
       if(fail)
         BFAM_LDEBUG("Fail match %25.15"BFAM_REAL_PRIe
             " %25.15"BFAM_REAL_PRIe " %d",
-            f_m[idx], f_p[idx],
-            BFAM_REAL_ABS(f_m[idx]-f_p[idx]) < BFAM_REAL_MIN);
+            f_m[idx], fac*f_p[idx],
+            BFAM_REAL_ABS(f_m[idx]-fac*f_p[idx]) < BFAM_REAL_MIN);
 
       failures += fail;
     }
   }
+
+  if(failures > 0)
+    BFAM_WARNING("FAIL! %s",name);
+  return failures;
+}
+
+static int
+check_back(bfam_subdomain_dgx_quad_glue_t *sub, const char *name)
+{
+  int failures = 0;
+  bfam_real_t *f_m = bfam_dictionary_get_value_ptr(&sub->base.fields_m, name);
+  bfam_real_t *f_p = bfam_dictionary_get_value_ptr(&sub->base.fields_p, name);
+  bfam_real_t *sub_m_f   = bfam_dictionary_get_value_ptr(&sub->sub_m->base.fields,
+                                                   name);
+
+  BFAM_ASSERT(f_m != NULL);
+  BFAM_ASSERT(f_p != NULL);
+  BFAM_ASSERT(sub_m_f != NULL);
+
+  BFAM_LDEBUG("Testing subdomain (%2jd, %2jd) -- (%2jd, %2jd)",
+      (intmax_t)sub->rank_m, (intmax_t)sub->s_m,
+      (intmax_t)sub->rank_p, (intmax_t)sub->s_p);
 
   /* check the projection back */
   /*
@@ -270,24 +353,136 @@ build_mesh(MPI_Comm mpicomm)
 
   bfam_domain_add_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p1");
   bfam_domain_add_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p2");
+  bfam_domain_add_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p3");
+  bfam_domain_add_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p4");
+  bfam_domain_add_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p5");
+  bfam_domain_add_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p6");
+
   bfam_domain_add_minus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
       "p1");
   bfam_domain_add_minus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
       "p2");
+  bfam_domain_add_minus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
+      "p3");
+  bfam_domain_add_minus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
+      "p4");
+  bfam_domain_add_minus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
+      "p5");
+  bfam_domain_add_minus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
+      "p6");
+
   bfam_domain_add_plus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
       "p1");
   bfam_domain_add_plus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
       "p2");
+  bfam_domain_add_plus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
+      "p3");
+  bfam_domain_add_plus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
+      "p4");
+  bfam_domain_add_plus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
+      "p5");
+  bfam_domain_add_plus_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
+      "p6");
 
   bfam_domain_init_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p1",
       0, poly1_field, NULL);
-
   bfam_domain_init_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p2",
       0, poly2_field, NULL);
+  bfam_domain_init_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p3",
+      0, poly3_field, NULL);
+  bfam_domain_init_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p4",
+      0, poly4_field, NULL);
+  bfam_domain_init_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p5",
+      0, poly5_field, NULL);
+  bfam_domain_init_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p6",
+      0, poly6_field, NULL);
+
+  bfam_subdomain_comm_args_t commargs;
+
+  const char *comm_args_face_scalars[]      = {NULL};
+  const char *comm_args_scalars[]           = {"p1", "p2", "p3",
+                                               "p4", "p5", "p6", NULL};
+  const char *comm_args_vectors[]           = {"v",NULL};
+  const char *comm_args_vector_components[] = {"p1","p2","p3",NULL};
+  const char *comm_args_tensors[]           = {"T",NULL};
+  const char *comm_args_tensor_components[] = {"p1", "p2", "p3",
+                                               "p4", "p5", "p6", NULL};
+  commargs.scalars_m           = comm_args_scalars;
+  commargs.scalars_p           = comm_args_scalars;
+
+  commargs.vectors_m           = comm_args_vectors;
+  commargs.vectors_p           = comm_args_vectors;
+  commargs.vector_components_m = comm_args_vector_components;
+  commargs.vector_components_p = comm_args_vector_components;
+
+  commargs.tensors_m           = comm_args_tensors;
+  commargs.tensors_p           = comm_args_tensors;
+  commargs.tensor_components_m = comm_args_tensor_components;
+  commargs.tensor_components_p = comm_args_tensor_components;
+
+  commargs.face_scalars_m = comm_args_face_scalars;
+  commargs.face_scalars_p = comm_args_face_scalars;
+
+  /* add glue fields */
+  for(int f = 0 ; comm_args_scalars[f] != NULL; f++)
+  {
+    bfam_domain_add_minus_field((bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        comm_args_scalars[f]);
+    bfam_domain_add_plus_field( (bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        comm_args_scalars[f]);
+  }
+  for(int f = 0 ; comm_args_vectors[f] != NULL; f++)
+  {
+    char name[BFAM_BUFSIZ];
+    snprintf(name,BFAM_BUFSIZ, "%sn",comm_args_vectors[f]);
+    bfam_domain_add_minus_field((bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    bfam_domain_add_plus_field( (bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    snprintf(name,BFAM_BUFSIZ, "%sp1",comm_args_vectors[f]);
+    bfam_domain_add_minus_field((bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    bfam_domain_add_plus_field( (bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    snprintf(name,BFAM_BUFSIZ, "%sp2",comm_args_vectors[f]);
+    bfam_domain_add_minus_field((bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    bfam_domain_add_plus_field( (bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    snprintf(name,BFAM_BUFSIZ, "%sp3",comm_args_vectors[f]);
+    bfam_domain_add_minus_field((bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    bfam_domain_add_plus_field( (bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+  }
+  for(int f = 0 ; comm_args_tensors[f] != NULL; f++)
+  {
+    char name[BFAM_BUFSIZ];
+    snprintf(name,BFAM_BUFSIZ, "%sn",comm_args_tensors[f]);
+    bfam_domain_add_minus_field((bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    bfam_domain_add_plus_field( (bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    snprintf(name,BFAM_BUFSIZ, "%sp1",comm_args_tensors[f]);
+    bfam_domain_add_minus_field((bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    bfam_domain_add_plus_field( (bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    snprintf(name,BFAM_BUFSIZ, "%sp2",comm_args_tensors[f]);
+    bfam_domain_add_minus_field((bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    bfam_domain_add_plus_field( (bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    snprintf(name,BFAM_BUFSIZ, "%sp3",comm_args_tensors[f]);
+    bfam_domain_add_minus_field((bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+    bfam_domain_add_plus_field( (bfam_domain_t*) domain, BFAM_DOMAIN_OR, glue,
+        name);
+  }
 
   bfam_communicator_t* communicator =
     bfam_communicator_new((bfam_domain_t*)domain, BFAM_DOMAIN_OR, glue,
-        mpicomm, 10, NULL);
+        mpicomm, 10, &commargs);
 
   /* start recv_send */
   bfam_communicator_start(communicator);
@@ -295,11 +490,10 @@ build_mesh(MPI_Comm mpicomm)
   /* finish recv */
   bfam_communicator_finish(communicator);
 
-  const char *ps[] = {"p1", "p2", NULL};
+  const char *ps[] = {"p1", "p2", "p3", "p4", "p5", "p6", NULL};
 
   bfam_vtk_write_file((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume,
                        "","ps",0, ps, NULL, NULL, 0, 0, 0);
-
 
   /*
    * Check to see if neighboring values got communicated
@@ -319,9 +513,49 @@ build_mesh(MPI_Comm mpicomm)
     for(bfam_locidx_t s = 0; s < numSubdomains; ++s)
     {
       failures +=
-        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p1");
+        check_back((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p1");
       failures +=
-        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p2");
+        check_back((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p2");
+      failures +=
+        check_back((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p3");
+      failures +=
+        check_back((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p4");
+      failures +=
+        check_back((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p5");
+      failures +=
+        check_back((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p6");
+
+      /* last argument lets us change the sign if necessary */
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p1", 1);
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p2", 1);
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p3", 1);
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p4", 1);
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p5", 1);
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "p6", 1);
+
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "Tn",   1);
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "Tp1", -1);
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "Tp2", -1);
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "Tp3", -1);
+
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "vn", -1);
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "vp1", 1);
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "vp2", 1);
+      failures +=
+        check_pm((bfam_subdomain_dgx_quad_glue_t*)subdomains[s], "vp3", 1);
     }
 
     bfam_free(subdomains);
@@ -411,6 +645,7 @@ main (int argc, char *argv[])
 
   bfam_gopt_free(options);
 
+  if(failures > 0) BFAM_WARNING("FAIL! with failures %d",failures);
   return failures;
 }
 
