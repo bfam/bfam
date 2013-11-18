9a33aa12a19f3bb37aa5c32085b89b490d3c9d07
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-11-18 15:18:22 -0800

    Start of the generic dgx subdomain

---
+++ src/bfam_subdomain_dgx.c
@@ -0,0 +1,7 @@
+#include <bfam_subdomain_dgx.h>
+#include <bfam_jacobi.h>
+#include <bfam_kron.h>
+#include <bfam_log.h>
+#include <bfam_util.h>
+#include <bfam_vtk.h>
+
---
+++ src/bfam_subdomain_dgx.h
@@ -0,0 +1,7 @@
+#ifndef BFAM_SUBDOMAIN_DGX_H
+#define BFAM_SUBDOMAIN_DGX_H
+
+#include <bfam_base.h>
+#include <bfam_subdomain.h>
+
+#endif
