84605281dc81fda39754e81213492f7f8746f06f
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-11-19 11:14:39 -0800

    start of generic dgx init and new

--- src/bfam_subdomain_dgx.c
+++ src/bfam_subdomain_dgx.c
@@ -5,3 +5,61 @@
 #include <bfam_util.h>
 #include <bfam_vtk.h>
 
+#ifndef BFAM_DGX_DIMENSION
+#define BFAM_DGX_DIMENSION
+#define USE_GENERIC_DGX_DIMENSION
+#else
+#define DIM (BFAM_DGX_DIMENSION)
+#endif
+
+void
+BFAM_APPEND_EXPAND(bfam_subdomain_dgx_init_,BFAM_DGX_DIMENSION)(
+                              bfam_subdomain_dgx_t *subdomain,
+                        const bfam_locidx_t         id,
+                        const char                 *name,
+                        const int                   N,
+                        const bfam_locidx_t         Nv,
+                        const int                   num_Vi,
+                        const bfam_long_real_t    **Vi,
+                        const bfam_locidx_t         K,
+                        const bfam_locidx_t        *EToV,
+                        const bfam_locidx_t        *EToE,
+                        const int8_t               *EToF,
+                        const int                   inDIM)
+{
+#ifdef USE_GENERIC_DGX_DIMENSION
+  BFAM_WARNING("Using generic bfam_subdomain_dgx_init");
+  const int DIM = inDIM;
+#endif
+  BFAM_ASSERT(DIM == inDIM);
+
+}
+
+bfam_subdomain_dgx_t*
+BFAM_APPEND_EXPAND(bfam_subdomain_dgx_new_,BFAM_DGX_DIMENSION)(
+                       const bfam_locidx_t      id,
+                       const char              *name,
+                       const int                N,
+                       const bfam_locidx_t      Nv,
+                       const int                num_Vi,
+                       const bfam_long_real_t **Vi,
+                       const bfam_locidx_t      K,
+                       const bfam_locidx_t     *EToV,
+                       const bfam_locidx_t     *EToE,
+                       const int8_t            *EToF,
+                       const int                inDIM)
+{
+#ifdef USE_GENERIC_DGX_DIMENSION
+  BFAM_WARNING("Using generic bfam_subdomain_dgx_new");
+  const int DIM = inDIM;
+#endif
+  BFAM_ASSERT(DIM == inDIM);
+
+  bfam_subdomain_dgx_t* newSubdomain =
+    bfam_malloc(sizeof(bfam_subdomain_dgx_t));
+
+  BFAM_APPEND_EXPAND(bfam_subdomain_dgx_init_,BFAM_DGX_DIMENSION)(
+                         newSubdomain, id, name, N, Nv, num_Vi, Vi, K, EToV,
+                         EToE, EToF,DIM);
+  return newSubdomain;
+}
--- src/bfam_subdomain_dgx.h
+++ src/bfam_subdomain_dgx.h
@@ -41,7 +41,7 @@ typedef struct bfam_subdomain_dgx
 {
   bfam_subdomain_t base;
 
-  int            dim;          /* dimensionality of this dgx */
+  int              dim;        /* dimensionality of this dgx */
 
   int              N;          /* 1D Polynomial Order */
   int              Np;         /* Number of points in the element */
@@ -58,24 +58,25 @@ typedef struct bfam_subdomain_dgx
   bfam_real_t     *exact_mass; /* exact mass matrix for this grid */
 
 
-  bfam_real_t     *Dr;       /* 1D LGL differentiation matrix */
+  bfam_real_t      *Dr;      /* 1D LGL differentiation matrix */
 
   bfam_long_real_t *V;       /* 1D Vandermonde matrix for this N */
 
-  bfam_locidx_t    K;        /* Number of elements in the subdomain */
+  bfam_locidx_t     K;       /* Number of elements in the subdomain */
 
-  bfam_locidx_t   *vmapM;    /* Mapping into the volume for the minus side of
+  bfam_locidx_t    *vmapM;   /* Mapping into the volume for the minus side of
                                 the face mesh */
-  bfam_locidx_t   *vmapP;    /* Mapping into the volume for the plus  side of
+  bfam_locidx_t    *vmapP;   /* Mapping into the volume for the plus  side of
                                 the face mesh */
 
-  int            **fmask;    /* face mask */
+  int             **fmask;   /* face mask */
 
   /* glue quantities
    * all glue quantities are postfixed with _m for minus side or _p for plus
    * side. All _p quantities are arrays to allow for multiple plus side
    * subdomains
    */
+  int             num_p;    /* number of plus sides */
   int               N_m;    /* 1D Polynomial Order on the minus side */
   int              *N_p;    /* 1D Polynomial Order on the plus  side */
 
@@ -119,12 +120,108 @@ typedef struct bfam_subdomain_dgx
                                        * to be the identity operator.
                                        */
 
-  bfam_locidx_t    *EToEp_m; /* Element     number on neighboring glue */
-  bfam_locidx_t    *EToEm_m; /* Element     number on local subdomain */
-  int8_t           *EToFm_m; /* Face        number on local subdomain */
-  int8_t           *EToHm_m; /* Hanging     number on local subdomain */
-  int8_t           *EToOm_m; /* Orientation number on local subdomain */
+  bfam_locidx_t   **EToE_p; /* Element     number on neighboring glue */
+  bfam_locidx_t    *EToE_m; /* Element     number on local subdomain */
+  int8_t           *EToF_m; /* Face        number on local subdomain */
+  int8_t           *EToH_m; /* Hanging     number on local subdomain */
+  int8_t           *EToO_m; /* Orientation number on local subdomain */
 } bfam_subdomain_dgx_t;
 
+/** create a dgx subdomain.
+ *
+ * \param [in] id     unique id number for this subdomain
+ * \param [in] name   name of this subdomain
+ * \param [in] N      polynomial order of elements in each dimension
+ * \param [in] Nv     number of vertices in the subdomain
+ * \param [in] num_Vi number of coordinate for vertices to store
+ * \param [in] Vi     array of array of coordinates for the vertives
+ * \param [in] K      number of elements in the subdomain
+ * \param [in] EToV   Mapping such that \c EToV[k*4+c] gives the vertex number
+ *                    for corner \c c of element \c k.
+ * \param [in] EToE   Mapping such that \c EToE[k*4+f] gives the element number
+ *                    connected to face \c f of element \c k.
+ * \param [in] EToF   Mapping such that \c EToF[k*4+f] gives the
+ *                    face+orientation number connected to face \c f of element
+ *                    \c k.
+ * \param [in] dim    number of (computational) dimensions
+ *
+ * \return Initialized dgx subdomain
+ *
+ */
+
+bfam_subdomain_dgx_t*
+bfam_subdomain_dgx_new_(const bfam_locidx_t      id,
+                       const char              *name,
+                       const int                N,
+                       const bfam_locidx_t      Nv,
+                       const int                num_Vi,
+                       const bfam_long_real_t **Vi,
+                       const bfam_locidx_t      K,
+                       const bfam_locidx_t     *EToV,
+                       const bfam_locidx_t     *EToE,
+                       const int8_t            *EToF,
+                       const int                dim);
+
+/** initializes a dgx subdomain
+ *
+ * \param [in,out] subdomain pointer to the subdomain to initialize
+ * \param [in]     id        unique id number for this subdomain
+ * \param [in]     name      name of this subdomain
+ * \param [in]     N         polynomial order of elements in each dimension
+ * \param [in]     Nv        number of vertices in the subdomain
+ * \param [in]     num_Vi    number of coordinate for vertices to store
+ * \param [in]     Vi        array of array of coordinates for the vertives
+ * \param [in]     K         number of elements in the subdomain
+ * \param [in]     EToV      Mapping such that \c EToV[k*4+c] gives the vertex
+ *                           number for corner \c c of element \c k.
+ * \param [in]     EToE      Mapping such that \c EToE[k*4+f] gives the element
+ *                           number connected to face \c f of element \c k.
+ * \param [in]     EToF      Mapping such that \c EToF[k*4+f] gives the
+ *                           face+orientation number connected to face \c f of
+ *                           element \c k.
+ * \param [in]     dim       number of (computational) dimensions
+ */
+void
+bfam_subdomain_dgx_init_(bfam_subdomain_dgx_t *subdomain,
+                        const bfam_locidx_t        id,
+                        const char                *name,
+                        const int                  N,
+                        const bfam_locidx_t        Nv,
+                        const int                  num_Vi,
+                        const bfam_long_real_t   **Vi,
+                        const bfam_locidx_t        K,
+                        const bfam_locidx_t       *EToV,
+                        const bfam_locidx_t       *EToE,
+                        const int8_t              *EToF,
+                        const int                  dim);
+
+#define X(dg_dim) \
+bfam_subdomain_dgx_t*                                         \
+bfam_subdomain_dgx_new_##dg_dim(const bfam_locidx_t      id,  \
+                             const char              *name,   \
+                             const int                N,      \
+                             const bfam_locidx_t      Nv,     \
+                             const int                num_Vi, \
+                             const bfam_long_real_t **Vi,     \
+                             const bfam_locidx_t      K,      \
+                             const bfam_locidx_t     *EToV,   \
+                             const bfam_locidx_t     *EToE,   \
+                             const int8_t            *EToF,   \
+                             const int                dim);   \
+void                                                              \
+bfam_subdomain_dgx_init_##dg_dim(bfam_subdomain_dgx_t *subdomain, \
+                        const bfam_locidx_t        id,            \
+                        const char                *name,          \
+                        const int                  N,             \
+                        const bfam_locidx_t        Nv,            \
+                        const int                  num_Vi,        \
+                        const bfam_long_real_t   **Vi,            \
+                        const bfam_locidx_t        K,             \
+                        const bfam_locidx_t       *EToV,          \
+                        const bfam_locidx_t       *EToE,          \
+                        const int8_t              *EToF,          \
+                        const int                  dim);
+BFAM_LIST_OF_BGX_DIMENSIONS
+#undef X
 
 #endif
