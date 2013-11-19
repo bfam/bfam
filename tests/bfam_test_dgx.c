fb412de21872dbaf2d3499deaa90a706c50e04ca
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-11-19 11:16:22 -0800

    start of test for dgx

---
+++ tests/bfam_test_dgx.c
@@ -0,0 +1,14 @@
+#include <bfam.h>
+
+int
+main (int argc, char *argv[])
+{
+
+  bfam_subdomain_dgx_new_0(1, NULL, 1, 1, 1, NULL, 1, NULL, NULL, NULL, 0);
+  bfam_subdomain_dgx_new_1(1, NULL, 1, 1, 1, NULL, 1, NULL, NULL, NULL, 1);
+  bfam_subdomain_dgx_new_2(1, NULL, 1, 1, 1, NULL, 1, NULL, NULL, NULL, 2);
+  bfam_subdomain_dgx_new_3(1, NULL, 1, 1, 1, NULL, 1, NULL, NULL, NULL, 3);
+
+  return 0;
+}
+
