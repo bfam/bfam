79ce045a299fe913d864887e656574fbc66eb1ac
Lucas C Wilcox <lucas@swirlee.com>
2013-11-25 10:59:53 -0800

    Fix test of subdomain order
    We should actually test the order based on ids and not the memory
    address.

--- tests/bfam_test_domain.c
+++ tests/bfam_test_domain.c
@@ -69,9 +69,9 @@ test_insert()
     domain->numSubdomains, matchedSubdomains, &numMatchedSubdomains);
   BFAM_ABORT_IF(numMatchedSubdomains != 3, "Error matching tags1: %jd",
       (intmax_t) numMatchedSubdomains);
-  BFAM_ABORT_IF(matchedSubdomains[0] > matchedSubdomains[1],
+  BFAM_ABORT_IF(matchedSubdomains[0]->id > matchedSubdomains[1]->id,
       "Error matching tags1 order");
-  BFAM_ABORT_IF(matchedSubdomains[1] > matchedSubdomains[2],
+  BFAM_ABORT_IF(matchedSubdomains[1]->id > matchedSubdomains[2]->id,
       "Error matching tags1 order");
 
   const char *tags2[] = {"a11", "testing 1 2 3", NULL};
@@ -88,9 +88,9 @@ test_insert()
     domain->numSubdomains, matchedSubdomains, &numMatchedSubdomains);
   BFAM_ABORT_IF(numMatchedSubdomains != 3, "Error matching ctags1: %jd",
       (intmax_t) numMatchedSubdomains);
-  BFAM_ABORT_IF(matchedSubdomains[0] > matchedSubdomains[1],
+  BFAM_ABORT_IF(matchedSubdomains[0]->id > matchedSubdomains[1]->id,
       "Error matching ctags1 order");
-  BFAM_ABORT_IF(matchedSubdomains[1] > matchedSubdomains[2],
+  BFAM_ABORT_IF(matchedSubdomains[1]->id > matchedSubdomains[2]->id,
       "Error matching ctags1 order");
   bfam_critbit0_clear(&ctags1);
 
