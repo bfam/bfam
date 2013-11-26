b0651024f51b3c220d5e7b58ffd035bea086c949
Lucas C Wilcox <lucas@swirlee.com>
2013-11-26 10:56:04 -0800

    Add test for p8est dgx
    Note that this doesn't test anything explicitly right now but when
    compiled in debug mode the communication pattern will be tested.

---
+++ tests/bfam_test_mesh_dgx_3.c
@@ -0,0 +1,207 @@
+#include <bfam.h>
+
+#define REAL_APPROX_EQ(x, y, K)                                              \
+  BFAM_APPROX_EQ((x), (y), (K), BFAM_REAL_ABS, BFAM_REAL_EPS, BFAM_REAL_EPS)
+
+static int          refine_level = 0;
+
+static int
+refine_fn(p8est_t* p8est, p4est_locidx_t which_tree, p8est_quadrant_t* quadrant)
+{
+  if ((int)quadrant->level >= refine_level - (int)(1 - which_tree % 2))
+    return 0;
+
+  return 1;
+}
+
+static void
+poly1_field(bfam_locidx_t npoints, const char* name,
+    bfam_real_t time, bfam_real_t *restrict x, bfam_real_t *restrict y,
+    bfam_real_t *restrict z, struct bfam_subdomain *s, void *arg,
+    bfam_real_t *restrict field)
+{
+  BFAM_ASSUME_ALIGNED(x, 32);
+  BFAM_ASSUME_ALIGNED(y, 32);
+  BFAM_ASSUME_ALIGNED(z, 32);
+  BFAM_ASSUME_ALIGNED(field, 32);
+
+  for(bfam_locidx_t n=0; n < npoints; ++n)
+    field[n] = -x[n];
+}
+
+static int
+build_mesh(MPI_Comm mpicomm)
+{
+  int failures = 0;
+  int rank;
+  BFAM_MPI_CHECK(MPI_Comm_rank(mpicomm, &rank));
+
+  p8est_connectivity_t *conn = p8est_connectivity_new_rotcubes();
+
+  bfam_domain_p8est_t* domain = bfam_domain_p8est_new(mpicomm, conn);
+
+  refine_level = 2;
+  p8est_refine(domain->p8est, 2, refine_fn, NULL);
+  p8est_balance(domain->p8est, P8EST_CONNECT_CORNER, NULL);
+  p8est_partition(domain->p8est, NULL);
+
+  p8est_vtk_write_file(domain->p8est, NULL, "p8est_mesh");
+
+  bfam_locidx_t numSubdomains = 2;
+  bfam_locidx_t *subdomainID =
+    bfam_malloc(domain->p8est->local_num_quadrants*sizeof(bfam_locidx_t));
+  bfam_locidx_t *N = bfam_malloc(numSubdomains*sizeof(int));
+
+  /*
+   * Create an arbitrary splitting of the domain to test things.
+   *
+   * When use a subdomain id independent of MPI partition.  In practice
+   * the subdomain id will be selected based on physics, element type, element
+   * order, etc.
+   *
+   * For no particular reason increase element order with id
+   */
+  BFAM_ROOT_INFO("Splitting p8est into %jd DG Quad subdomains",
+      (intmax_t) numSubdomains);
+  for(bfam_locidx_t id = 0; id < numSubdomains; ++id)
+  {
+    N[id] = 4+id;
+
+    p4est_gloidx_t first =
+      p4est_partition_cut_gloidx(domain->p8est->global_num_quadrants,
+          id, numSubdomains);
+
+    p4est_gloidx_t last =
+      p4est_partition_cut_gloidx(domain->p8est->global_num_quadrants,
+          id + 1, numSubdomains) - 1;
+
+    BFAM_ROOT_INFO("  id:%jd N:%d GIDs:%jd--%jd", (intmax_t) id, N[id],
+        (intmax_t) first, (intmax_t) last);
+  }
+
+  p4est_gloidx_t gkOffset = domain->p8est->global_first_quadrant[rank];
+
+  bfam_locidx_t idStart = 0;
+  while(gkOffset >
+      p4est_partition_cut_gloidx(domain->p8est->global_num_quadrants,
+        idStart + 1, numSubdomains) - 1) ++idStart;
+
+  for(p4est_locidx_t lk = 0, id = idStart;
+      lk < domain->p8est->local_num_quadrants;
+      ++lk)
+  {
+    p4est_gloidx_t gk = gkOffset + lk;
+
+    if(gk > p4est_partition_cut_gloidx(domain->p8est->global_num_quadrants,
+                                       id + 1, numSubdomains) - 1)
+      ++id;
+
+    BFAM_ASSERT(
+      (gk >= p4est_partition_cut_gloidx(domain->p8est->global_num_quadrants,
+                                   id, numSubdomains)) &&
+      (gk < p4est_partition_cut_gloidx(domain->p8est->global_num_quadrants,
+                                   id + 1, numSubdomains)));
+
+    subdomainID[lk] = id;
+  }
+
+  bfam_domain_p8est_split_dgx_subdomains(domain, numSubdomains,
+      subdomainID, N);
+
+  const char *volume[] = {"_volume", NULL};
+
+  bfam_domain_add_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p1");
+
+  bfam_domain_init_field((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume, "p1",
+      0, poly1_field, NULL);
+
+  const char *ps[] = {"p1", NULL};
+
+  bfam_vtk_write_file((bfam_domain_t*)domain, BFAM_DOMAIN_OR, volume,
+                       "","ps",0, ps, NULL, NULL, 0, 0, 0);
+
+
+  /* clean up */
+  bfam_free(subdomainID);
+  bfam_free(N);
+
+  bfam_domain_p8est_free(domain);
+  bfam_free(domain);
+  p8est_connectivity_destroy(conn);
+
+  return failures;
+}
+
+int
+main (int argc, char *argv[])
+{
+  int failures = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  int rank;
+
+  void *options= bfam_gopt_sort(&argc, (const char**)argv,
+      bfam_gopt_start(
+        bfam_gopt_option('h', 0,
+                         bfam_gopt_shorts('h', '?'),
+                         bfam_gopt_longs("help", "HELP")),
+        bfam_gopt_option('V', 0,
+                         bfam_gopt_shorts('V'),
+                         bfam_gopt_longs("version")),
+        bfam_gopt_option('v', BFAM_GOPT_REPEAT,
+                         bfam_gopt_shorts('v'),
+                         bfam_gopt_longs("verbose"))
+        )
+      );
+
+  const char *helpText =
+  "\n"
+  "\n"
+  "  there are four possible options to this program, some of which have\n"
+  "  multiple names:\n"
+  "\n"
+  "    -h -? --help --HELP\n"
+  "    -V --version\n"
+  "    -v --verbose  (which may be repeated for more verbosity)\n"
+  "\n";
+
+  if(bfam_gopt(options, 'h'))
+  {
+    /*
+     * if any of the help options was specified
+     */
+    BFAM_ROOT_INFO(helpText);
+    exit(EXIT_SUCCESS);
+  }
+
+  if(bfam_gopt(options, 'V'))
+  {
+    BFAM_ROOT_INFO("BFAM Version: %s", bfam_version_get());
+    BFAM_ROOT_INFO("BFAM Compile Info:\n" BFAM_COMPILE_INFO);
+    exit( EXIT_SUCCESS );
+  }
+
+  int verbosity = bfam_gopt(options, 'v');
+
+  BFAM_MPI_CHECK(MPI_Init(&argc,&argv));
+  BFAM_MPI_CHECK(MPI_Comm_rank(comm, &rank));
+
+  int logLevel = BFAM_MAX(BFAM_LL_INFO - verbosity, BFAM_LL_ALWAYS);
+
+  bfam_log_init(rank, stdout, logLevel);
+  bfam_signal_handler_set();
+
+  int scLogPriorities = BFAM_MAX(SC_LP_STATISTICS - verbosity, SC_LP_ALWAYS);
+  sc_init(comm, 0, 0, NULL, scLogPriorities);
+  p4est_init(NULL, scLogPriorities);
+
+  failures += build_mesh(comm);
+
+  sc_finalize();
+  BFAM_MPI_CHECK(MPI_Finalize());
+
+  bfam_gopt_free(options);
+
+  if(failures > 0) BFAM_WARNING("FAIL! with failures %d",failures);
+  return failures;
+}
+
