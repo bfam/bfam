ff44e3c9a75280e30bbd3cbd3cc7404f0fe94988
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-12-02 11:25:05 -0800

    adding back in the preferences

--- examples/beard/beard.c
+++ examples/beard/beard.c
@@ -3,6 +3,194 @@
 #include "beard_dgx_rhs.h"
 #include <p4est_iterate.h>
 
+struct lsrk_table {
+  const char *name;
+  bfam_ts_lsrk_method_t lsrk_method;
+} lsrk_table[] = {
+  {"KC54", BFAM_TS_LSRK_KC54},
+  {"FE",   BFAM_TS_LSRK_FE},
+  {"HEUN", BFAM_TS_LSRK_HEUN},
+  {"W33",  BFAM_TS_LSRK_W33},
+  {NULL,   BFAM_TS_LSRK_NOOP},
+};
+
+typedef struct brick_args
+{
+  int nx;
+  int ny;
+  int nz;
+  int periodic_x;
+  int periodic_y;
+  int periodic_z;
+} brick_args_t;
+
+/* Lua help functions */
+static int
+lua_get_global_int(lua_State *L, const char *name, int def)
+{
+  lua_getglobal(L, name);
+  int result = def;
+  if(!lua_isnumber(L, -1))
+    BFAM_ROOT_WARNING("`%s' not found, using default %d", name, def);
+  else
+    result = (int)lua_tonumber(L, -1);
+  lua_pop(L, 1);
+  return result;
+}
+
+static int
+lua_get_table_int(lua_State *L, const char *table, const char *name, int def)
+{
+  int result = def;
+  lua_getglobal(L, table);
+  if(!lua_istable(L, -1))
+    BFAM_ROOT_WARNING("table `%s' not found, using default %d", table, def);
+  else
+  {
+    lua_pushstring(L,name);
+    lua_gettable(L,-2);
+    if(!lua_isnumber(L,-1))
+      BFAM_ROOT_WARNING("table `%s' does not contain `%s', using default %d",
+          table, name, def);
+    else
+      result = (int)lua_tonumber(L, -1);
+    lua_pop(L, 1);
+  }
+  lua_pop(L, 1);
+  return result;
+}
+
+typedef struct prefs
+{
+  lua_State *L;
+
+  int dimension;
+
+  char output_prefix[BFAM_BUFSIZ];
+
+  brick_args_t* brick_args;
+
+  bfam_ts_lsrk_method_t lsrk_method;
+  char lsrk_name[BFAM_BUFSIZ];
+} prefs_t;
+
+/*
+ * Set up the beard preference file
+ */
+static prefs_t *
+new_prefs(const char *prefs_filename)
+{
+  prefs_t *prefs = bfam_malloc(sizeof(prefs_t));
+
+  lua_State *L = luaL_newstate();
+  luaL_openlibs(L);
+
+  if(luaL_loadfile(L, prefs_filename) || lua_pcall(L, 0, 0, 0))
+    BFAM_LERROR("cannot run configuration file: `%s'", lua_tostring(L, -1));
+
+  prefs->L = L;
+
+  BFAM_ASSERT(lua_gettop(L)==0);
+
+  prefs->dimension = lua_get_global_int(prefs->L,"dimension",2);
+
+  BFAM_ABORT_IF_NOT(prefs->dimension == 2 || prefs->dimension == 3,
+      "beard not set up to handle dimension %d", prefs->dimension);
+
+  /* get output prefix */
+  lua_getglobal(L,"output_prefix");
+  strncpy(prefs->output_prefix,"solution",BFAM_BUFSIZ);
+  if(lua_isstring(L, -1))
+  {
+    const char *output_prefix = lua_tostring(L, -1);
+    strncpy(prefs->output_prefix,output_prefix,BFAM_BUFSIZ);
+  }
+  else
+    BFAM_ROOT_WARNING("using default 'output_prefix': %s",prefs->output_prefix);
+
+  /* get the time stepper type */
+  lua_getglobal(L, "lsrk_method");
+  prefs->lsrk_method = lsrk_table[0].lsrk_method;
+  strncpy(prefs->lsrk_name,lsrk_table[0].name,BFAM_BUFSIZ);
+  if(lua_isstring(L, -1))
+  {
+    int i;
+    const char *lsrk_name = lua_tostring(L, -1);
+    for(i = 0; lsrk_table[i].name != NULL; ++i)
+    {
+      if(strcmp(lsrk_name, lsrk_table[i].name) == 0)
+        break;
+    }
+
+    if(lsrk_table[i].name == NULL)
+      BFAM_ROOT_WARNING("invalid lsrk method name: `%s'; using default %s",
+          lsrk_name, prefs->lsrk_name);
+    else
+    {
+      prefs->lsrk_method = lsrk_table[i].lsrk_method;
+      strncpy(prefs->lsrk_name,lsrk_table[i].name,BFAM_BUFSIZ);
+    }
+  }
+  else
+  {
+    BFAM_ROOT_WARNING("`lsrk method' not found, using default: %s",
+        prefs->lsrk_name);
+  }
+  BFAM_ASSERT(prefs->lsrk_method != BFAM_TS_LSRK_NOOP);
+  lua_pop(L, 1);
+
+  /* get the connectivity type */
+  prefs->brick_args     = bfam_malloc(sizeof(brick_args_t));
+
+  prefs->brick_args->nx = lua_get_table_int(prefs->L, "brick", "nx",1);
+  prefs->brick_args->ny = lua_get_table_int(prefs->L, "brick", "ny",1);
+  if(prefs->dimension > 2)
+    prefs->brick_args->nz = lua_get_table_int(prefs->L, "brick", "nz",1);
+  else
+    prefs->brick_args->nz = 1;
+
+  prefs->brick_args->periodic_x = lua_get_table_int(prefs->L, "brick",
+      "periodic_x",1);
+  prefs->brick_args->periodic_y = lua_get_table_int(prefs->L, "brick",
+      "periodic_y",1);
+  if(prefs->dimension > 2)
+    prefs->brick_args->periodic_z = 
+      lua_get_table_int(prefs->L, "brick", "periodic_z",1);
+  else
+    prefs->brick_args->periodic_z = 1;
+
+  return prefs;
+}
+
+static void
+print_prefs(prefs_t *prefs)
+{
+  BFAM_ROOT_INFO("----------Preferences----------");
+  BFAM_ROOT_INFO(" Beard Dimension          = %d",prefs->dimension);
+  BFAM_ROOT_INFO(" Output prefix            = %s",prefs->output_prefix);
+  BFAM_ROOT_INFO(" Low Storage Time Stepper = %s",prefs->lsrk_name);
+  if(prefs->brick_args != NULL)
+  {
+    BFAM_ROOT_INFO(" brick arguments");
+    BFAM_ROOT_INFO("  nx         = %d", prefs->brick_args->nx);
+    BFAM_ROOT_INFO("  ny         = %d", prefs->brick_args->ny);
+    if(prefs->dimension > 2)
+      BFAM_ROOT_INFO("  nz         = %d", prefs->brick_args->nz);
+    BFAM_ROOT_INFO("  periodic_x = %d", prefs->brick_args->periodic_x);
+    BFAM_ROOT_INFO("  periodic_y = %d", prefs->brick_args->periodic_y);
+    if(prefs->dimension > 2)
+      BFAM_ROOT_INFO("  periodic_z = %d", prefs->brick_args->periodic_z);
+  }
+  BFAM_ROOT_INFO("-------------------------------");
+}
+
+static void
+free_prefs(prefs_t *prefs)
+{
+  if(prefs->brick_args != NULL) bfam_free(prefs->brick_args);
+  lua_close(prefs->L);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -71,13 +259,13 @@ main(int argc, char *argv[])
   sc_init(comm, 0, 0, NULL, sc_log_priorities);
   p4est_init(NULL, sc_log_priorities);
 
-  // prefs_t *prefs = new_prefs(argv[1]);
-  // print_prefs(prefs);
+  prefs_t *prefs = new_prefs(argv[1]);
+  print_prefs(prefs);
 
   // run(comm, prefs);
 
-  // free_prefs(prefs);
-  // bfam_free(prefs);
+  free_prefs(prefs);
+  bfam_free(prefs);
 
   sc_finalize();
   BFAM_MPI_CHECK(MPI_Finalize());
