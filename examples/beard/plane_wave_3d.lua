e2c8e5ab1904fd427d271c38809079ce58747a84
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-12-03 09:27:06 -0800

    Just running the test out farther

--- examples/beard/plane_wave_3d.lua
+++ examples/beard/plane_wave_3d.lua
@@ -143,14 +143,14 @@ end
 -- time stepper to use
 lsrk_method  = "KC54"
 
--- tend  = Lx*5/c_s
--- tout  = tend/100
--- tdisp = tend/100
--- terr  = tend/100
-tend  = Lx*1/c_s
-tout  = tend/10
-tdisp = tend/10
-terr  = tend
+tend  = Lx*5/c_s
+tout  = tend/100
+tdisp = tend/100
+terr  = tend/100
+-- tend  = Lx*1/c_s
+-- tout  = tend/10
+-- tdisp = tend/10
+-- terr  = tend
 
 function time_step_parameters(dt)
   dt      = 0.5*dt
