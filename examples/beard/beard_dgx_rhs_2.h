5b6707b41eae655200c9d9068c01d1d3ff29e938
Jeremy Edward Kozdon <jekozdon@nps.edu>
2013-12-05 15:08:12 -0800

    Adding prototype to header

--- examples/beard/beard_dgx_rhs_2.h
+++ examples/beard/beard_dgx_rhs_2.h
@@ -3,26 +3,36 @@
 
 #include <bfam.h>
 
-#define X(order) \
+#define X(order)                                                     \
   void beard_dgx_intra_rhs_elastic_2_##order(int N,                  \
-      bfam_subdomain_dgx_t *sub, const char *rate_prefix,     \
-      const char *field_prefix, const bfam_long_real_t t);         \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const char *field_prefix, const bfam_long_real_t t);           \
   void beard_dgx_scale_rates_elastic_2_##order(int N,                \
-      bfam_subdomain_dgx_t *sub, const char *rate_prefix,     \
-      const bfam_long_real_t a);                                   \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const bfam_long_real_t a);                                     \
   void beard_dgx_add_rates_elastic_2_##order(int N,                  \
-      bfam_subdomain_dgx_t *sub, const char *field_prefix_lhs,\
-      const char *field_prefix_rhs, const char *rate_prefix,       \
-      const bfam_long_real_t a);                                   \
+      bfam_subdomain_dgx_t *sub, const char *field_prefix_lhs,       \
+      const char *field_prefix_rhs, const char *rate_prefix,         \
+      const bfam_long_real_t a);                                     \
+  void beard_dgx_scale_rates_slip_weakening_2_##order(int N,         \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const bfam_long_real_t a);                                     \
+  void beard_dgx_add_rates_slip_weakening_2_##order(int N,           \
+      bfam_subdomain_dgx_t *sub, const char *field_prefix_lhs,       \
+      const char *field_prefix_rhs, const char *rate_prefix,         \
+      const bfam_long_real_t a);                                     \
   void beard_dgx_inter_rhs_boundary_2_##order(int N,                 \
-      bfam_subdomain_dgx_t *sub, const char *rate_prefix,\
-      const char *field_prefix, const bfam_long_real_t t,          \
-      const bfam_real_t R);                                        \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const char *field_prefix, const bfam_long_real_t t,            \
+      const bfam_real_t R);                                          \
   void beard_dgx_inter_rhs_interface_2_##order(int N,                \
-      bfam_subdomain_dgx_t *sub, const char *rate_prefix,\
-      const char *field_prefix, const bfam_long_real_t t);         \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const char *field_prefix, const bfam_long_real_t t);           \
+  void beard_dgx_inter_rhs_slip_weakening_interface_2_##order(int N, \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const char *field_prefix, const bfam_long_real_t t);           \
   void beard_dgx_energy_2_##order(int N,                             \
-      bfam_real_t* energy_sq, bfam_subdomain_dgx_t *sub,      \
+      bfam_real_t* energy_sq, bfam_subdomain_dgx_t *sub,             \
       const char *field_prefix);
 BFAM_LIST_OF_DGX_NORDERS
 #undef X
@@ -37,10 +47,20 @@ void beard_dgx_add_rates_elastic_2_(int N,
     bfam_subdomain_dgx_t *sub, const char *field_prefix_lhs,
     const char *field_prefix_rhs, const char *rate_prefix,
     const bfam_long_real_t a);
+void beard_dgx_scale_rates_slip_weakening_2_(int N,
+    bfam_subdomain_dgx_t *sub, const char *rate_prefix,
+    const bfam_long_real_t a);
+void beard_dgx_add_rates_slip_weakening_2_(int N,
+    bfam_subdomain_dgx_t *sub, const char *field_prefix_lhs,
+    const char *field_prefix_rhs, const char *rate_prefix,
+    const bfam_long_real_t a);
 void beard_dgx_inter_rhs_boundary_2_(int N,
     bfam_subdomain_dgx_t *sub, const char *rate_prefix,
     const char *field_prefix, const bfam_long_real_t t, const bfam_real_t R);
 void beard_dgx_inter_rhs_interface_2_(int N,
+    bfam_subdomain_dgx_t *sub, const char *rate_prefix,
+    const char *field_prefix, const bfam_long_real_t t);
+void beard_dgx_inter_rhs_slip_weakening_interface_2_(int N,
     bfam_subdomain_dgx_t *sub, const char *rate_prefix,
     const char *field_prefix, const bfam_long_real_t t);
 void beard_dgx_energy_2_(int N, bfam_real_t *energy_sq,
--- examples/beard/beard_dgx_rhs_3.h
+++ examples/beard/beard_dgx_rhs_3.h
@@ -3,26 +3,36 @@
 
 #include <bfam.h>
 
-#define X(order) \
+#define X(order)                                                     \
   void beard_dgx_intra_rhs_elastic_3_##order(int N,                  \
-      bfam_subdomain_dgx_t *sub, const char *rate_prefix,     \
-      const char *field_prefix, const bfam_long_real_t t);         \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const char *field_prefix, const bfam_long_real_t t);           \
   void beard_dgx_scale_rates_elastic_3_##order(int N,                \
-      bfam_subdomain_dgx_t *sub, const char *rate_prefix,     \
-      const bfam_long_real_t a);                                   \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const bfam_long_real_t a);                                     \
   void beard_dgx_add_rates_elastic_3_##order(int N,                  \
-      bfam_subdomain_dgx_t *sub, const char *field_prefix_lhs,\
-      const char *field_prefix_rhs, const char *rate_prefix,       \
-      const bfam_long_real_t a);                                   \
+      bfam_subdomain_dgx_t *sub, const char *field_prefix_lhs,       \
+      const char *field_prefix_rhs, const char *rate_prefix,         \
+      const bfam_long_real_t a);                                     \
+  void beard_dgx_scale_rates_slip_weakening_3_##order(int N,         \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const bfam_long_real_t a);                                     \
+  void beard_dgx_add_rates_slip_weakening_3_##order(int N,           \
+      bfam_subdomain_dgx_t *sub, const char *field_prefix_lhs,       \
+      const char *field_prefix_rhs, const char *rate_prefix,         \
+      const bfam_long_real_t a);                                     \
   void beard_dgx_inter_rhs_boundary_3_##order(int N,                 \
-      bfam_subdomain_dgx_t *sub, const char *rate_prefix,\
-      const char *field_prefix, const bfam_long_real_t t,          \
-      const bfam_real_t R);                                        \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const char *field_prefix, const bfam_long_real_t t,            \
+      const bfam_real_t R);                                          \
   void beard_dgx_inter_rhs_interface_3_##order(int N,                \
-      bfam_subdomain_dgx_t *sub, const char *rate_prefix,\
-      const char *field_prefix, const bfam_long_real_t t);         \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const char *field_prefix, const bfam_long_real_t t);           \
+  void beard_dgx_inter_rhs_slip_weakening_interface_3_##order(int N, \
+      bfam_subdomain_dgx_t *sub, const char *rate_prefix,            \
+      const char *field_prefix, const bfam_long_real_t t);           \
   void beard_dgx_energy_3_##order(int N,                             \
-      bfam_real_t* energy_sq, bfam_subdomain_dgx_t *sub,      \
+      bfam_real_t* energy_sq, bfam_subdomain_dgx_t *sub,             \
       const char *field_prefix);
 BFAM_LIST_OF_DGX_NORDERS
 #undef X
@@ -37,10 +47,20 @@ void beard_dgx_add_rates_elastic_3_(int N,
     bfam_subdomain_dgx_t *sub, const char *field_prefix_lhs,
     const char *field_prefix_rhs, const char *rate_prefix,
     const bfam_long_real_t a);
+void beard_dgx_scale_rates_slip_weakening_3_(int N,
+    bfam_subdomain_dgx_t *sub, const char *rate_prefix,
+    const bfam_long_real_t a);
+void beard_dgx_add_rates_slip_weakening_3_(int N,
+    bfam_subdomain_dgx_t *sub, const char *field_prefix_lhs,
+    const char *field_prefix_rhs, const char *rate_prefix,
+    const bfam_long_real_t a);
 void beard_dgx_inter_rhs_boundary_3_(int N,
     bfam_subdomain_dgx_t *sub, const char *rate_prefix,
     const char *field_prefix, const bfam_long_real_t t, const bfam_real_t R);
 void beard_dgx_inter_rhs_interface_3_(int N,
+    bfam_subdomain_dgx_t *sub, const char *rate_prefix,
+    const char *field_prefix, const bfam_long_real_t t);
+void beard_dgx_inter_rhs_slip_weakening_interface_3_(int N,
     bfam_subdomain_dgx_t *sub, const char *rate_prefix,
     const char *field_prefix, const bfam_long_real_t t);
 void beard_dgx_energy_3_(int N, bfam_real_t *energy_sq,
