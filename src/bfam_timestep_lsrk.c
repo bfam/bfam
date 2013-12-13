9df317dbc83333d3b5f0bf1c8aaa1404fb8a3893
Lucas C Wilcox <lucas@swirlee.com>
2013-12-13 13:52:28 -0800

    Fix precision of numeric constants in ts lsrk

--- src/bfam_timestep_lsrk.c
+++ src/bfam_timestep_lsrk.c
@@ -152,7 +152,7 @@ bfam_ts_lsrk_init(bfam_ts_lsrk_t* ts,
    */
   bfam_ts_init(&ts->base, dom);
   bfam_dictionary_init(&ts->elems);
-  ts->t  = 0.0;
+  ts->t  = BFAM_LONG_REAL(0.0);
   ts->base.step = &bfam_ts_lsrk_step;
 
   /*
@@ -194,24 +194,37 @@ bfam_ts_lsrk_init(bfam_ts_lsrk_t* ts,
       ts->B = bfam_malloc_aligned(ts->nStages*sizeof(bfam_long_real_t));
       ts->C = bfam_malloc_aligned((ts->nStages+1)*sizeof(bfam_long_real_t));
 
-      ts->A[0] = 0;
-      ts->A[1] = -567301805773.0/1357537059087.0;
-      ts->A[2] = -2404267990393.0/2016746695238.0;
-      ts->A[3] = -3550918686646.0/2091501179385.0;
-      ts->A[4] = -1275806237668.0/842570457699.0;
+      ts->A[0] = BFAM_LONG_REAL(0.0);
+      ts->A[1] = BFAM_LONG_REAL(-567301805773.0)/
+                 BFAM_LONG_REAL(1357537059087.0);
+      ts->A[2] = BFAM_LONG_REAL(-2404267990393.0)/
+                 BFAM_LONG_REAL(2016746695238.0);
+      ts->A[3] = BFAM_LONG_REAL(-3550918686646.0)/
+                 BFAM_LONG_REAL(2091501179385.0);
+      ts->A[4] = BFAM_LONG_REAL(-1275806237668.0)/
+                 BFAM_LONG_REAL(842570457699.0);
 
-      ts->B[0] = 1432997174477.0/9575080441755.0;
-      ts->B[1] = 5161836677717.0/13612068292357.0;
-      ts->B[2] = 1720146321549.0/2090206949498.0;
-      ts->B[3] = 3134564353537.0/4481467310338.0;
-      ts->B[4] = 2277821191437.0/14882151754819.0;
+      ts->B[0] = BFAM_LONG_REAL(1432997174477.0)/
+                 BFAM_LONG_REAL(9575080441755.0);
+      ts->B[1] = BFAM_LONG_REAL(5161836677717.0)/
+                 BFAM_LONG_REAL(13612068292357.0);
+      ts->B[2] = BFAM_LONG_REAL(1720146321549.0)/
+                 BFAM_LONG_REAL(2090206949498.0);
+      ts->B[3] = BFAM_LONG_REAL(3134564353537.0)/
+                 BFAM_LONG_REAL(4481467310338.0);
+      ts->B[4] = BFAM_LONG_REAL(2277821191437.0)/
+                 BFAM_LONG_REAL(14882151754819.0);
 
-      ts->C[0] = 0.0;
-      ts->C[1] = 1432997174477.0/9575080441755.0;
-      ts->C[2] = 2526269341429.0/6820363962896.0;
-      ts->C[3] = 2006345519317.0/3224310063776.0;
-      ts->C[4] = 2802321613138.0/2924317926251.0;
-      ts->C[5] = 1.0;
+      ts->C[0] = BFAM_LONG_REAL(0.0);
+      ts->C[1] = BFAM_LONG_REAL(1432997174477.0)/
+                 BFAM_LONG_REAL(9575080441755.0);
+      ts->C[2] = BFAM_LONG_REAL(2526269341429.0)/
+                 BFAM_LONG_REAL(6820363962896.0);
+      ts->C[3] = BFAM_LONG_REAL(2006345519317.0)/
+                 BFAM_LONG_REAL(3224310063776.0);
+      ts->C[4] = BFAM_LONG_REAL(2802321613138.0)/
+                 BFAM_LONG_REAL(2924317926251.0);
+      ts->C[5] = BFAM_LONG_REAL(1.0);
       break;
     case BFAM_TS_LSRK_W33:
       ts->nStages = 3;
@@ -219,18 +232,18 @@ bfam_ts_lsrk_init(bfam_ts_lsrk_t* ts,
       ts->B = bfam_malloc_aligned(ts->nStages*sizeof(bfam_long_real_t));
       ts->C = bfam_malloc_aligned((ts->nStages+1)*sizeof(bfam_long_real_t));
 
-      ts->A[0] = 0.0;
-      ts->A[1] = -5.0/9.0;
-      ts->A[2] = -153.0/128.0;
+      ts->A[0] = BFAM_LONG_REAL(0.0);
+      ts->A[1] = BFAM_LONG_REAL(-5.0)/BFAM_LONG_REAL(9.0);
+      ts->A[2] = BFAM_LONG_REAL(-153.0)/BFAM_LONG_REAL(128.0);
 
-      ts->B[0] = 1.0/3.0;
-      ts->B[1] = 15.0/16.0;
-      ts->B[2] = 8.0/15.0;
+      ts->B[0] = BFAM_LONG_REAL(1.0)/BFAM_LONG_REAL(3.0);
+      ts->B[1] = BFAM_LONG_REAL(15.0)/BFAM_LONG_REAL(16.0);
+      ts->B[2] = BFAM_LONG_REAL(8.0)/BFAM_LONG_REAL(15.0);
 
-      ts->C[0] = 0.0;
-      ts->C[1] = 1.0/3.0;
-      ts->C[2] = 3.0/4.0;
-      ts->C[3] = 1.0;
+      ts->C[0] = BFAM_LONG_REAL(0.0);
+      ts->C[1] = BFAM_LONG_REAL(1.0)/BFAM_LONG_REAL(3.0);
+      ts->C[2] = BFAM_LONG_REAL(3.0)/BFAM_LONG_REAL(4.0);
+      ts->C[3] = BFAM_LONG_REAL(1.0);
       break;
     case BFAM_TS_LSRK_HEUN:
       ts->nStages = 2;
@@ -238,15 +251,15 @@ bfam_ts_lsrk_init(bfam_ts_lsrk_t* ts,
       ts->B = bfam_malloc_aligned(ts->nStages*sizeof(bfam_long_real_t));
       ts->C = bfam_malloc_aligned((ts->nStages+1)*sizeof(bfam_long_real_t));
 
-      ts->A[0] = 0.0;
-      ts->A[1] = -1.0;
+      ts->A[0] = BFAM_LONG_REAL(0.0);
+      ts->A[1] = -BFAM_LONG_REAL(1.0);
 
-      ts->B[0] = 1.0;
-      ts->B[1] = 1.0/2.0;
+      ts->B[0] = BFAM_LONG_REAL(1.0);
+      ts->B[1] = BFAM_LONG_REAL(1.0)/BFAM_LONG_REAL(2.0);
 
-      ts->C[0] = 0.0;
-      ts->C[1] = 1.0;
-      ts->C[2] = 1.0;
+      ts->C[0] = BFAM_LONG_REAL(0.0);
+      ts->C[1] = BFAM_LONG_REAL(1.0);
+      ts->C[2] = BFAM_LONG_REAL(1.0);
       break;
     case BFAM_TS_LSRK_FE:
       ts->nStages = 1;
@@ -254,12 +267,12 @@ bfam_ts_lsrk_init(bfam_ts_lsrk_t* ts,
       ts->B = bfam_malloc_aligned(sizeof(bfam_long_real_t));
       ts->C = bfam_malloc_aligned((ts->nStages+1)*sizeof(bfam_long_real_t));
 
-      ts->A[0] = 0.0;
+      ts->A[0] = BFAM_LONG_REAL(0.0);
 
-      ts->B[0] = 1.0;
+      ts->B[0] = BFAM_LONG_REAL(1.0);
 
-      ts->C[0] = 0.0;
-      ts->C[1] = 1.0;
+      ts->C[0] = BFAM_LONG_REAL(0.0);
+      ts->C[1] = BFAM_LONG_REAL(1.0);
       break;
   }
 }
