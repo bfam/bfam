95922fa777d758278e2916b5f686a43c4e473e18
Jeremy Edward Kozdon <jekozdon@nps.edu>
2014-12-05 12:53:54 -0800

    updated process fault script

--- examples/beard/SCEC/matlab_tools/process_fault_station.m
+++ examples/beard/SCEC/matlab_tools/process_fault_station.m
@@ -1,21 +1,25 @@
-function data = process_fault_station(base_name, outputname, directory)
+% n_ref is the positive normal direction. All that is used is the sign of
+% n_ref \cdot n and using this, if n = [0,1,0] and n_ref=[0,1,0]:
+%      vP1 = vxP -  vxM
+%      Vp2 = 0 (no opening)
+%      vP3 = vzP -  vzM
+function [pdata,data] = process_fault_station(n_ref,base_name, directory)
 
-if nargin < 3
-  directory = '.';
-end
 if nargin < 2
-  outputname = [base_name,'.scec'];
+  directory = '.';
 end
 
 A = dir(directory);
 
-data = [];
+data(2).name = []; % kills an mlint warning
 
-cnt = 0;
+variables = {'t','Tp1','Tp2','Tp3','Tn','V','Vp1','Vp2','Vp3','Dp','Dp1','Dp2','Dp3','Dn'};
+
+N_data = 0;
 for k = 1:length(A)
-  if strncmpi(base_name,A(k).name,length(base_name)) && 1 ~=  strcmp(outputname,A(k).name)
-    cnt = cnt + 1;
-    data(cnt).name = A(k).name;
+  if strncmpi(base_name,A(k).name,length(base_name))
+    N_data = N_data + 1;
+    data(N_data).name = A(k).name;
 
     full_name = [directory,'/',A(k).name];
 
@@ -25,67 +29,88 @@ for k = 1:length(A)
     ln = fgetl(iid);
     while ln(1) == '#'
       header = header + 1;
+      data(N_data).header{header} = ln;
       ln = fgetl(iid);
       if strcmp(ln,'# interpolated normal:') == 1
         header = header + 1;
         ln = fgetl(iid);
-        data(cnt).n = sscanf(ln,'#    (%e, %e, %e)' )';
+        data(N_data).n = sscanf(ln,'#    (%e, %e, %e)' )';
       end
     end
 
-    data(cnt).variables = ln;
+    data(N_data).variables = ln;
     num_col = 14;
-    if strcmp(data(cnt).variables,'t Tp1 Tp2 Tp3 Tn V Vp1 Vp2 Vp3 Dp Dp1 Dp2 Dp3 Dn') ~= 1
+    if strcmp(data(N_data).variables,'t Tp1 Tp2 Tp3 Tn V Vp1 Vp2 Vp3 Dp Dp1 Dp2 Dp3 Dn') ~= 1
       error('Format changed')
     end
     dat = fscanf(iid,'%e');
-    data(cnt).dat = reshape(dat,num_col,length(dat)/num_col)';
+    data(N_data).dat = reshape(dat,num_col,length(dat)/num_col)';
 
     fclose(iid);
+
+    for n = 1:length(variables)
+      eval(['data(N_data).',variables{n},' = data(N_data).dat(:,n);']);
+    end
   end
 end
+data = data(1:N_data);
 
-[t,Tp1,Tp2,Tp3,Tn,V,Vp1,Vp2,Vp3,Dp,Dp1,Dp2,Dp3,Dn] = deal(1,2,3,4,5,6,7,8,9,10,11,12,13,14);
-if cnt > 0
-  M = length(data(1).dat(:,t));
-  for k = 2:cnt
-    M = min(M,length(data(k).dat(:,t)));
-  end
-  t = data(1).dat(1:M,t);
-  hslip        = zeros(size(t));
-  hsliprate    = zeros(size(t));
-  hshearstress = zeros(size(t));
-  vslip        = zeros(size(t));
-  vsliprate    = zeros(size(t));
-  vshearstress = zeros(size(t));
-  nstress      = zeros(size(t));
-  for k = 1:cnt
-    vshearstress = vshearstress - data(cnt).dat(1:M,Tp3)/cnt/data(cnt).n(2);
-    vsliprate    = vsliprate    + data(cnt).dat(1:M,Vp3)/cnt;
-    vslip        = vslip        - data(cnt).dat(1:M,Dp3)/cnt;
-
-    hshearstress = hshearstress + data(cnt).dat(1:M,Tp1)/cnt/data(cnt).n(2);
-    hsliprate    = hsliprate    - data(cnt).dat(1:M,Vp1)/cnt;
-    hslip        = hslip        - data(cnt).dat(1:M,Dp1)/cnt;
-
-    nstress      = nstress      + data(cnt).dat(1:M,Tn )/cnt;
+if N_data > 0
+  n_steps = inf;
+  for n = 1:N_data
+    n_steps = min(n_steps,length(data(n).t));
   end
 
-  oid = fopen(outputname,'w');
-  iid = fopen(full_name, 'r');
-  ln = fgetl(iid);
-  while ln(1) == '#'
-    fprintf(oid,'%s\n',ln);
-    ln = fgetl(iid);
-  end
-  fclose(iid);
+  pdata.t = data(1).t(1:n_steps);
 
-  fprintf(oid,'t  h-slip  h-slip-rate  h-shear-stress  v-slip  v-slip-rate v-shear-stress  n-stress\n');
-  for k = 2:length(t)
-    fprintf(oid,'%+30.17E %+30.17E %+30.17E %+30.17E %+30.17E %+30.17E %+30.17E %+30.17E\n',...
-             t(k),hslip(k),hsliprate(k),hshearstress(k),vslip(k),vsliprate(k),vshearstress(k),nstress(k));
+
+  pdata.Dp  = 0;
+  pdata.Dp1 = 0;
+  pdata.Dp2 = 0;
+  pdata.Dp3 = 0;
+  pdata.Dn  = 0;
+
+  pdata.V   = 0;
+  pdata.Vp1 = 0;
+  pdata.Vp2 = 0;
+  pdata.Vp3 = 0;
+
+  pdata.Tn  = 0;
+  pdata.Tp1 = 0;
+  pdata.Tp2 = 0;
+  pdata.Tp3 = 0;
+
+  pdata.n   = sign(n_ref(1)*data(1).n(1) + n_ref(2)*data(1).n(2))*data(1).n;
+  head_saved = false;
+  for n = 1:N_data
+    n_sign    = sign(n_ref(1)*data(n).n(1) + n_ref(2)*data(n).n(2));
+    if(~head_saved && n_sign == 1)
+      head_saved = true;
+      pdata.header = data(n).header;
+    end
+    if(pdata.n ~= n_sign*data(n).n)
+      warning(['Normals maybe discontinuous (rethink averaging)!'...
+               'May want to work with tractions directly????'])
+      disp(pdata.n)
+      disp(n_sign*data(n).n)
+    end
+
+    pdata.Dp  = pdata.Dp  +        data(n).Dp /N_data;
+    pdata.Dp1 = pdata.Dp1 + n_sign*data(n).Dp1/N_data;
+    pdata.Dp2 = pdata.Dp2 + n_sign*data(n).Dp2/N_data;
+
+    pdata.V   = pdata.V   +        data(n).V  /N_data;
+    pdata.Vp1 = pdata.Vp1 + n_sign*data(n).Vp1/N_data;
+    pdata.Vp2 = pdata.Vp2 + n_sign*data(n).Vp2/N_data;
+
+    pdata.Tn  = pdata.Tn  +        data(n).Tn /N_data;
+    pdata.Tp1 = pdata.Tp1 + n_sign*data(n).Tp1/N_data;
+    pdata.Tp2 = pdata.Tp2 + n_sign*data(n).Tp2/N_data;
+
   end
-  fclose(oid);
+  pdata.Tn (1) = pdata.Tn (2) ;
+  pdata.Tp1(1) = pdata.Tp1(2);
+  pdata.Tp2(1) = pdata.Tp2(2);
+else
+  pdata = [];
 end
-
-
