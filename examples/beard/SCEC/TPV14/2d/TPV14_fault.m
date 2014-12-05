c1a7a7eb2012baf9807a10525905ae4f1fea7d80
Jeremy Edward Kozdon <jekozdon@nps.edu>
2014-12-05 12:54:20 -0800

    Start to a TPV14 process fault file

---
+++ examples/beard/SCEC/TPV14/2d/TPV14_fault.m
@@ -0,0 +1,23 @@
+clear
+
+sim_name = 'TPV14_base_';
+names = {[sim_name,'faultst-020dp075'],...
+         [sim_name,'faultst020dp075'] ,...
+         [sim_name,'faultst050dp075'] ,...
+         [sim_name,'faultst090dp075'] ,...
+         [sim_name,'branchst020dp075'],...
+         [sim_name,'branchst050dp075'],...
+         [sim_name,'branchst090dp075']};
+
+for k = 1:length(names)
+  pd = process_fault_station([0,1],names{k},'data');
+
+  if(isempty(pd))
+    disp(['NOT FOUND :: ',names{k}])
+  else
+    disp(['    FOUND :: ',names{k}])
+    plot(pd.t,pd.V)
+    title(names{k},'interpreter','none')
+    pause
+  end
+end
