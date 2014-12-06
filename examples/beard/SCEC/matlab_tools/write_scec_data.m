36682b39fa767f468348956329dd2c4d7b48e36c
Jeremy Edward Kozdon <jekozdon@nps.edu>
2014-12-05 17:44:12 -0800

    Finishg of the write scec data

--- examples/beard/SCEC/matlab_tools/write_scec_data.m
+++ examples/beard/SCEC/matlab_tools/write_scec_data.m
@@ -1,9 +1,20 @@
-function write_scec_data(filename,header,fields,varargin)
+function write_scec_data(filename,header,fields,DAT)
 
 fid = fopen(filename,'w');
 
 for k = 1:length(header)
   fprintf(fid,[header{k},'\n']);
+end
+fprintf(fid,[fields,'\n']);
+
+for k = 1:size(DAT,1)
+  for n = 1:size(DAT,2)
+    fprintf(fid,'%+30.17E',DAT(k,n));
+    if(n ~= size(DAT,2))
+      fprintf(fid,' ');
+    end
+  end
+  fprintf(fid,'\n');
 end
 
 fclose(fid);
