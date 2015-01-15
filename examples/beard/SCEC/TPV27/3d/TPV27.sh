c0d5650e9d1ebd962a382bd959c799d6f865d272
Jeremy Edward Kozdon <jekozdon@nps.edu>
2015-01-15 13:34:20 -0800

    Fixing the launch script

--- examples/beard/SCEC/TPV27/3d/TPV27.sh
+++ examples/beard/SCEC/TPV27/3d/TPV27.sh
@@ -4,7 +4,8 @@
 #PBS -N TPV27
 # PBS -l hostlist=compute-2-1+compute-2-11+compute-2-13+compute-2-15+compute-2-17+compute-2-19+compute-2-3+compute-2-5+compute-2-7+compute-2-9
 # PBS -l hostlist=compute-3-1+compute-3-11+compute-3-13+compute-3-15+compute-3-17+compute-3-19+compute-3-21+compute-3-23+compute-3-25+compute-3-27+compute-3-29+compute-3-3+compute-3-31+compute-3-5+compute-3-7+compute-3-9
-#PBS -l hostlist=compute-7-1+compute-7-11+compute-7-13+compute-7-15+compute-7-17+compute-7-19+compute-7-21+compute-7-23+compute-7-25+compute-7-27+compute-7-29+compute-7-3+compute-7-31+compute-7-33+compute-7-35+compute-7-37+compute-7-39+compute-7-5+compute-7-7+compute-7-9
+# PBS -l hostlist=compute-7-1+compute-7-11+compute-7-13+compute-7-15+compute-7-17+compute-7-19+compute-7-21+compute-7-23+compute-7-25+compute-7-27+compute-7-29+compute-7-3+compute-7-31+compute-7-33+compute-7-35+compute-7-37+compute-7-39+compute-7-5+compute-7-7+compute-7-9
+#PBS -l hostlist=compute-7-1+compute-7-11+compute-7-13+compute-7-15+compute-7-17+compute-7-19+compute-7-21+compute-7-23+compute-7-25+compute-7-27+compute-7-29+compute-7-3+compute-7-31+compute-7-33+compute-7-35+compute-7-37+compute-7-39+compute-7-7+compute-7-9
 #PBS -l procs=512
 #PBS -l pmem=2gb
 #PBS -l naccesspolicy=singlejob
@@ -44,8 +45,8 @@ source /etc/profile
 module load compile/intel/13.0.0 mpi/openmpi/1.8
 root_dir=/home/jekozdon/data/SCEC_CVWS/TPV27/3d
 
-echo mkdir -p $root_dir
-mkdir -p $root_dir
+echo mkdir -p $root_dir/data
+mkdir -p $root_dir/data
 
 echo cd $root_dir
 cd $root_dir
