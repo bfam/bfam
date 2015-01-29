7a424333864336028c5f8f2b44fe8ba4d43b71a6
Jeremy Edward Kozdon <jekozdon@nps.edu>
2015-01-28 17:18:28 -0800

    Fixing height

--- examples/beard/SCEC/TPV32/3d/TPV32_base.lua
+++ examples/beard/SCEC/TPV32/3d/TPV32_base.lua
@@ -129,6 +129,9 @@ function refinement_function(
   if(level >= max_level) then
     return 0
   end
+  if y4 < 15 and level < height_split then
+    return 1
+  end
 
   r = fault_distance(x0,y0,z0)
   r = min(r,fault_distance(x1,y1,z1))
