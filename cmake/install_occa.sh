e656f4cfaf35cb982297eab7cae3ea4dcc77f5ab
Lucas C Wilcox <lucas@swirlee.com>
2015-03-11 19:33:21 -0700

    Add occa script to install

--- cmake/install_occa.sh
+++ cmake/install_occa.sh
@@ -4,10 +4,11 @@ set -x
 
 [ -z "$1" ] && echo "No argument supplied" && exit 1
 
-mkdir -p "$1"/lib "$1"/include
+mkdir -p "$1"/lib "$1"/include "$1"/scripts
 cp lib/libocca.so "$1"/lib
 if [ `uname` = "Darwin" ]; then
   # install_name_tool -id '@rpath/libocca.so' "$1"/lib/libocca.so
   install_name_tool -id "$1"/lib/libocca.so "$1"/lib/libocca.so
 fi
 cp -r include/* "$1"/include
+cp  scripts/shellTools.sh "$1"/scripts
