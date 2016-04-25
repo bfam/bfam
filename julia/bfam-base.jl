ae053d8aadcd5da189a79a09dcf19b4f005495e1
Jeremy Edward Kozdon <jekozdon@nps.edu>
2016-04-25 13:16:42 -0700

    Add the creation of a domain

--- julia/bfam-base.jl
+++ julia/bfam-base.jl
@@ -1,5 +1,11 @@
 using MPI
 
+type Domain
+    domain_ptr::Ptr{Void}
+    pxest_ptr::Ptr{Void}
+    Domain(domain_ptr, pxest_ptr) = new(domain_ptr, pxest_ptr)
+end
+
 const BFAM_LC_ALL     =  0
 const BFAM_LC_ROOT    =  1
 const BFAM_LL_DEFAULT = -1
@@ -14,6 +20,9 @@ const BFAM_LL_SILENT  =  7
 
 const _bfam_functions = Dict{Symbol, ASCIIString}(
     :bfam_init_helper_f => "bfam_init_helper_f",
+    :bfam_domain_pxest_new_ext_f => "bfam_domain_pxest_new_ext_f",
+    :bfam_domain_get_pxest => "bfam_domain_get_pxest",
+    :bfam_domain_pxest_balance => "bfam_domain_pxest_balance",
     )
 
 const _p4est_functions = Dict{Symbol, ASCIIString}(
@@ -72,4 +81,29 @@ function connectivity_brick(nx::Int , ny::Int , nz::Int,
                Ptr{Void}, (Cint, Cint, Cint, Cint, Cint, Cint),
                nx, ny, nz, px, py, pz)
 
+end
+
+# domain_new
+function domain_pxest_new_ext_f(comm, pxest_conn, min_quadrants,
+                                min_level, fill_uniform)
+  domain_ptr = ccall(bfam_domain_pxest_new_ext_f,
+                Ptr{Void},  # bfam_domain_pxest_t *
+                (Cint,      # int f_domcomm,
+                 Ptr{Void}, # p4est_connectivity_t *conn,
+                 Cint,      # int min_quadrants,
+                 Cint,      # int min_level,
+                 Cint),     # int fill_uniform);
+                comm.val, pxest_conn, min_quadrants, min_level, fill_uniform)
+  pxest_ptr = ccall(bfam_domain_get_pxest,
+                Ptr{Void},    # pxest_t *
+                (Ptr{Void},), # bfam_domain_pxest_t *domain
+                domain_ptr)
+  return Domain(domain_ptr, pxest_ptr)
+end
+
+function domain_balance(domain::Domain)
+  ccall(bfam_domain_pxest_balance,
+                Void,
+                (Ptr{Void},),
+                domain.domain_ptr)
 end
--- julia/bfam2d-test.jl
+++ julia/bfam2d-test.jl
@@ -10,6 +10,13 @@ bfam.init()
 
 conn = bfam.connectivity_brick(2, 3, false, true)
 
+comm = MPI.COMM_WORLD
+mesh_min_elements = 0
+mesh_min_level = 0
+domain = bfam.domain_pxest_new_ext_f(comm, conn, mesh_min_elements,
+                                              mesh_min_level, 1)
+                                              println(domain.pxest_ptr)
+bfam.domain_balance(domain)
 
 MPI.Finalized()
 
